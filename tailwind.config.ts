ed8053815f5f1814d2db39f7487e39ff8d6a7cb1
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-29 16:07:46 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        neutral: {
+          100: "#333333",
+          200: "#737373",
+          300: "#D9D9D9",
+          400: "#FAFAFA",
+          500: "#FFFFFF",
+        },
+        primary: {
+          100: "#633CFF",
+          200: "#BEADFF",
+          300: "#EFEBFF",
+        },
       },
+      fontFamily: {
+        sans: ["var(--font-roboto)"],
+        mono: ["var(--font-roboto-mono)"],
+      },
+    },
+    screens: {
+      md: "768px",
+      // => @media (min-width: 1440px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1440px) { ... }
+      xl: "1440px",
+      // => @media (min-width: 1440px) { ... }
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
