1e4bd4d9f875907a1da9571e9979577d8ab09dbe
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-06 01:38:17 -0800

    Create showMobileNav state and toggleMobileNav function

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
+"use client";
 import { useState } from "react";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
+  const [showMobileNav, setShowMobileNav] = useState(false);
 
   // toggle dark mode
   function toggleTheme() {
     setDarkMode((prevMode) => !prevMode);
   }
 
+  function toggleMobileNav() {
+    setShowMobileNav((prevState) => !prevState);
+  }
 
 
 
