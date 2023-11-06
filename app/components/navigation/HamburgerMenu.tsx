3bea96f50119e1737a7cb42200210a706efebc24
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-06 01:40:45 -0800

    Create HamburgerMenu.tsx

---
+++ app/components/navigation/HamburgerMenu.tsx
@@ -0,0 +1,45 @@
+import clsx from "clsx";
+
+export default function HamburgerMenu(props: any) {
+  return (
+    <button
+      className={clsx(
+        "z-30 flex flex-col justify-around items-center gap-1 w-[40px] h-[40px] py-[13px] px-2 group",
+        {
+          "border border-neutral-500": props.showMobileNav && props.darkMode,
+          "border border-neutral-100": props.showMobileNav && !props.darkMode,
+        }
+      )}
+      onClick={() => props.toggleMobileNav()}
+    >
+      <span
+        className={clsx(
+          "inline-block w-5 h-[2px] transition-transform duration-400",
+          {
+            "rotate-45 translate-y-1": props.showMobileNav,
+            "bg-neutral-400": props.showMobileNav && props.darkMode,
+            "bg-neutral-200": props.showMobileNav && !props.darkMode,
+            "bg-neutral-500": !props.showMobileNav && props.darkMode,
+            "bg-neutral-100": !props.showMobileNav && !props.darkMode,
+            "group-hover:bg-accent-200": props.darkMode,
+            "group-hover:bg-accent-100": !props.darkMode,
+          }
+        )}
+      ></span>
+      <span
+        className={clsx(
+          "inline-block w-5 h-[2px] transition-transform duration-400",
+          {
+            "-rotate-45 -translate-y-1": props.showMobileNav,
+            "bg-neutral-400": props.showMobileNav && props.darkMode,
+            "bg-neutral-200": props.showMobileNav && !props.darkMode,
+            "bg-neutral-500": !props.showMobileNav && props.darkMode,
+            "bg-neutral-100": !props.showMobileNav && !props.darkMode,
+            "group-hover:bg-accent-200": props.darkMode,
+            "group-hover:bg-accent-100": !props.darkMode,
+          }
+        )}
+      ></span>
+    </button>
+  );
+}
