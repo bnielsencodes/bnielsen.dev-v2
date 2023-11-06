f42fc53a033e16520fca159a1f9b5ae9431da445
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-06 12:01:02 -0800

    Update subheading font-size

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -6,7 +6,7 @@ export default function Header(props: any) {
     <header className={clsx("flex flex-col items-center justify-center h-[calc(100vh-56px)]", { "bg-neutral-100": props.darkMode, "bg-neutral-500": !props.darkMode })}>
       <div className="hero-img"></div>
       <h1 className={clsx("font-mono text-3xl", { "text-neutral-500": props.darkMode, "text-neutral-100": !props.darkMode })}>Brandon Nielsen</h1>
-      <h2 className={clsx("", { "text-neutral-400": props.darkMode, "text-neutral-200": !props.darkMode })}>Web Developer</h2>
+      <h2 className={clsx("text-lg", { "text-neutral-400": props.darkMode, "text-neutral-200": !props.darkMode })}>Web Developer</h2>
     </header>
   );
 }
