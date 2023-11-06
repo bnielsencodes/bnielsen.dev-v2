36b9e9d1f5132407ae4da997def3653ad3b30544
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-06 09:28:38 -0800

    Update bg-color for dark mode

--- app/components/navigation/NavBar.tsx
+++ app/components/navigation/NavBar.tsx
@@ -2,10 +2,11 @@ import Image from "next/image";
 import darkLogo from "/public/assets/Main-Logo-800x600.png";
 import lightLogo from "/public/assets/Inverted-Color-800x600.png";
 import HamburgerMenu from "./HamburgerMenu";
+import clsx from "clsx";
 
 export default function NavBar(props: any) {
   return (
-    <nav className="z-10 flex items-center justify-between h-14 pl-4 pr-2 border-b border-neutral-300">
+    <nav className={clsx("z-10 flex items-center justify-between h-14 pl-4 pr-2 border-b border-neutral-300", { "bg-neutral-500": props.darkMode })}>
       {/* logo and name */}
       <div className="flex">
         <Image
