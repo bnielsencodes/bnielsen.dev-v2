72b003bb7acba88aa50f9c92fa6e9d088071cd58
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-06 15:46:13 -0800

    Removed calc height as navbar is now fixed position

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 export default function Header(props: any) {
   return (
     // this is a comment
-    <header className={clsx("flex flex-col items-center justify-center h-[calc(100vh-56px)]", { "bg-neutral-100": props.darkMode, "bg-neutral-500": !props.darkMode })}>
+    <header className={clsx("flex flex-col items-center justify-center h-14", { "bg-neutral-100": props.darkMode, "bg-neutral-500": !props.darkMode })}>
       <div className="hero-img"></div>
       <h1 className={clsx("mt-3 mb-[6px] font-mono text-3xl", { "text-neutral-500": props.darkMode, "text-neutral-100": !props.darkMode })}>Brandon Nielsen</h1>
       <h2 className={clsx("text-lg", { "text-neutral-400": props.darkMode, "text-neutral-200": !props.darkMode })}>Web Developer</h2>
