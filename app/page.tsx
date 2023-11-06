e9fe1529fe2d2fd9f505ae2b9f15918bc8b166a5
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-06 01:37:50 -0800

    Create darkMode state and toggleTheme function

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(true);
 
+  // toggle dark mode
+  function toggleTheme() {
+    setDarkMode((prevMode) => !prevMode);
+  }
 
 
 
