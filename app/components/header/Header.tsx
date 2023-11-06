57e275f4986a4206c255d97372db9e575a3f126b
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-06 09:58:12 -0800

    Update Header height
    Updated value from "h-screen" to "h-[calc(screen-56px)]" to account for navbar height.

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 export default function Header(props: any) {
   return (
     // this is a comment
-    <header className={clsx("h-screen", { "bg-neutral-100": props.darkMode, "bg-neutral-500": !props.darkMode })}>
+    <header className={clsx("h-[calc(screen-56px)]", { "bg-neutral-100": props.darkMode, "bg-neutral-500": !props.darkMode })}>
     </header>
   );
 }
