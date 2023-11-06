e67f147bead343eedce2cb3ec6c19c91718c9295
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-06 11:34:46 -0800

    Add hero-img animated blob shape

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -4,6 +4,7 @@ export default function Header(props: any) {
   return (
     // this is a comment
     <header className={clsx("h-[calc(100vh-56px)]", { "bg-neutral-100": props.darkMode, "bg-neutral-500": !props.darkMode })}>
+      <div className="hero-img"></div>
     </header>
   );
 }
