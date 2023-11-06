6c769b6bb1f70a2939ffe01e004024b2ffa0ca77
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-06 09:47:35 -0800

    Update Header.tsx

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 
 export default function Header(props: any) {
   return (
+    // this is a comment
     <header className={clsx("h-screen", { "bg-neutral-100": props.darkMode, "bg-neutral-500": !props.darkMode })}>
     </header>
   );
