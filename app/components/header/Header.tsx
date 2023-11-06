fec6a94942537b7114606e9129b6a927fcaa055a
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-06 11:52:03 -0800

    Add text to header, and flex header items

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -3,8 +3,10 @@ import clsx from "clsx";
 export default function Header(props: any) {
   return (
     // this is a comment
-    <header className={clsx("h-[calc(100vh-56px)]", { "bg-neutral-100": props.darkMode, "bg-neutral-500": !props.darkMode })}>
+    <header className={clsx("flex items-center justify-center h-[calc(100vh-56px)]", { "bg-neutral-100": props.darkMode, "bg-neutral-500": !props.darkMode })}>
       <div className="hero-img"></div>
+      <h1 className={clsx("", { "text-neutral-500": props.darkMode, "text-neutral-100": !props.darkMode })}>Brandon Nielsen</h1>
+      <h2 className={clsx("", { "text-neutral-400": props.darkMode, "text-neutral-200": !props.darkMode })}>Web Developer</h2>
     </header>
   );
 }
