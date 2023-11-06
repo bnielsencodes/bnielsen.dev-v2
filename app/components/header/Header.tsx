c58eb29eb5c9662260c0842e9ca6209d59762c17
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-06 12:07:32 -0800

    Update margin-top and margin-bottom for heading

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header(props: any) {
     // this is a comment
     <header className={clsx("flex flex-col items-center justify-center h-[calc(100vh-56px)]", { "bg-neutral-100": props.darkMode, "bg-neutral-500": !props.darkMode })}>
       <div className="hero-img"></div>
-      <h1 className={clsx("font-mono text-3xl", { "text-neutral-500": props.darkMode, "text-neutral-100": !props.darkMode })}>Brandon Nielsen</h1>
+      <h1 className={clsx("mt-3 mb-[6px] font-mono text-3xl", { "text-neutral-500": props.darkMode, "text-neutral-100": !props.darkMode })}>Brandon Nielsen</h1>
       <h2 className={clsx("text-lg", { "text-neutral-400": props.darkMode, "text-neutral-200": !props.darkMode })}>Web Developer</h2>
     </header>
   );
