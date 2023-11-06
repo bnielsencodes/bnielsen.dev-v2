e4a5c0e0cbb367fcc9bd0bf01bc591ec63ff0ee0
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-06 09:42:34 -0800

    Fix props

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 
-export default function Header() {
+export default function Header(props: any) {
   return (
     <header className={clsx("h-screen", { "bg-neutral-100": props.darkMode, "bg-neutral-500": !props.darkMode })}>
     </header>
