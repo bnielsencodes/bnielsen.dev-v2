80a8201d3a128bed42459f79e74360e504c47e93
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-10 22:25:31 -0800

    Update colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,20 +10,59 @@ const config: Config = {
     colors: {
       neutral: {
         100: "#1a1a1a",
-        200: "#555",
-        300: "#666",
+        200: "#272727",
+        300: "#555",
         400: "#888",
-        500: "#FAFAFA",
+        500: "#ededed",
+        600: "#fafafa",
       },
       accent: {
-        100: "#0072BB",
-        200: "#00C8F5",
+        100: "#0072bb",
+        200: "#00c8f5",
       },
     },
     extend: {
       fontFamily: {
         sans: ["var(--font-roboto)"],
         mono: ["var(--font-roboto-mono)"],
+      tech: {
+        css: {
+          100: "#1572b6",
+          200: "#33a9dc",
+          300: "#fff",
+          400: "#ebebeb",
+          500: "#fff",
+          600: "#ebebeb",
+        },
+        figma: {
+          100: "#0acf83",
+          200: "#a259ff",
+          300: "#f24e1e",
+          400: "#ff7262",
+          500: "#1abcfe",
+        },
+        git: "#f1512f",
+        html: {
+          100: "#e44d26",
+          200: "#f16529",
+          300: "#ebebeb",
+          400: "#fafafa",
+        },
+        javascript: {
+          100: "#f0db4f",
+          200: "#272727",
+        },
+        next: "#fff",
+        react: "#62dafb",
+        sass: "#c76494",
+        tailwind: "#36bdf9",
+        vite: {
+          100: "#41d1ff",
+          200: "#bd34fe",
+          300: "#ffea83",
+          400: "#ffdd35",
+          500: "#ffa800",
+        },
       },
     },
     screens: {
