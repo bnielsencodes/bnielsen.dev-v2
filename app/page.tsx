c25cffacd9e07051d00e14411192cead75ddd765
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-10 22:26:43 -0800

    Add wave section dividers

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 "use client";
 import { useState } from "react";
+import Image from "next/image";
+import waveDarkTop from "@/public/assets/dividers/wave-dark-top.svg";
+import waveLightTop from "@/public/assets/dividers/wave-light-top.svg";
+import waveDarkBottom from "@/public/assets/dividers/wave-dark-bottom.svg";
+import waveLightBottom from "@/public/assets/dividers/wave-light-bottom.svg";
 import NavBar from "./components/navigation/NavBar";
 import Header from "./components/header/Header";
 import Skills from "./components/skills/Skills";
@@ -18,6 +23,24 @@ export default function Home() {
         <NavBar darkMode={darkMode} toggleTheme={toggleTheme} />
         <Header darkMode={darkMode} />
         <Skills />
+        {/* section top divider */}
+        <Image
+          className="w-full -mb-[1px]"
+          src={darkMode ? waveDarkTop : waveLightTop}
+          alt="wave section divider"
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+        {/* section bottom divider */}
+        <Image
+          className="w-full -mt-[1px]"
+          src={darkMode ? waveDarkBottom : waveLightBottom}
+          alt="wave section divider"
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
       </main>
     </>
   );
