37bb94e221e8f566e6ed4c509a86ce671591eaef
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-12 00:33:04 -0800

    Import useScreenSize hook

--- app/components/about/skills/Skills.tsx
+++ app/components/about/skills/Skills.tsx
@@ -1,4 +1,8 @@
+import useScreenSize from "../../../lib/useScreenSize";
+
 export default function Skills(props: any) {
+  const screenSize = useScreenSize();
+
   return (
     <section className="w-[293px] mx-auto">
       <p className="w-[105px] border-b-4 border-b-neutral-300 mx-auto text-neutral-500 text-[1.4rem] font-semibold">
