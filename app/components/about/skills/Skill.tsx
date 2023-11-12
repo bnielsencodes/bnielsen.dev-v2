c587d84ae2ab3bc6b637eaac4723f907ffb8b466
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-12 00:29:29 -0800

    Create Skill.tsx

---
+++ app/components/about/skills/Skill.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+
+export default function Skill(props: any) {
+  return (
+    <div className="relative group">
+      <div
+        className={clsx(
+          "relative z-10 flex flex-col items-center justify-center gap-[5px] py-[18px] border-2 rounded-lg hover:cursor-help",
+          {
+          }
+        )}
+      >
+      </div>
+    </div>
+  );
+}
