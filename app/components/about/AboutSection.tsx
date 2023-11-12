07bfb3f6b5f3f0d6541acf96149bbccc61bdefbd
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-11 21:20:05 -0800

    Add heading

--- app/components/about/AboutSection.tsx
+++ app/components/about/AboutSection.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 export default function AboutSection(props: any) {
   return (
     <section
@@ -6,6 +8,18 @@ export default function AboutSection(props: any) {
         "bg-neutral-500": !props.darkMode,
       })}
     >
+      {/* section heading */}
+      <p
+        className={clsx(
+          "relative z-0 w-[155px] pt-[70px] mx-auto mb-[10px] text-[2.35rem] font-bold text-center after:absolute after:content-[''] after:-z-10 after:right-[3px] after:bottom-[5px] after:w-[102px] after:h-[14px]",
+          {
+            "text-neutral-500 after:bg-neutral-300": props.darkMode,
+            "text-neutral-200 after:bg-neutral-400": !props.darkMode,
+          }
+        )}
+      >
+        About
+      </p>
     </section>
   );
 }
