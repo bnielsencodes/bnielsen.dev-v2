12429267014174806547bf166fbe4f3222ae58a3
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-11 21:14:37 -0800

    Create useScreenSize.tsx

---
+++ app/lib/useScreenSize.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+interface ScreenSize {
+  width: number;
+  height: number;
+}
+
+const useScreenSize = (): ScreenSize => {
+  const [screenSize, setScreenSize] = useState<ScreenSize>({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return screenSize;
+};
+
+export default useScreenSize;
