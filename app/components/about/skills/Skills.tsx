bfb2a4959ba4c94ec0357c4252eac1e48a3ac8d3
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-12 00:27:42 -0800

    Add header

--- app/components/about/skills/Skills.tsx
+++ app/components/about/skills/Skills.tsx
@@ -1,6 +1,9 @@
 export default function Skills(props: any) {
   return (
-    <section className="grid grid-cols-3 gap-x-3 gap-y-4 w-[293px] mx-auto">
+    <section className="w-[293px] mx-auto">
+      <p className="w-[105px] border-b-4 border-b-neutral-300 mx-auto text-neutral-500 text-[1.4rem] font-semibold">
+        Top Skills
+      </p>
     </section>
   );
 }
