5bea22ff957d2249bb9dab702c0c4121919f81aa
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-12 17:25:53 -0800

    Update section headings

--- app/components/about-section/AboutSection.tsx
+++ app/components/about-section/AboutSection.tsx
@@ -5,14 +5,15 @@ import clsx from "clsx";
 export default function AboutSection(props: any) {
   return (
     <section
-      className={clsx("overflow-x-clip", {
+      id="about-section"
+      className={clsx("pt-20 pb-14 overflow-x-clip", {
         "bg-neutral-200": props.darkMode,
         "bg-neutral-500": !props.darkMode,
       })}
     >
       <p
         className={clsx(
-          "relative z-0 w-[145px] pt-[70px] mx-auto mb-[10px] text-[2.5rem] font-bold text-center after:absolute after:content-[''] after:-z-10 after:right-0 after:bottom-[5px] after:w-[102px] after:h-[14px]",
+          "relative z-0 w-[145px] mx-auto mb-[10px] text-[2.5rem] font-bold text-center after:absolute after:content-[''] after:-z-10 after:right-0 after:bottom-[5px] after:w-[102px] after:h-[14px]",
           {
             "text-neutral-500 after:bg-neutral-300": props.darkMode,
             "text-neutral-200 after:bg-neutral-400": !props.darkMode,
