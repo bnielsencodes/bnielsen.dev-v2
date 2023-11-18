8418cdacdba12aee1baa5f0480285cee987b1636
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-18 00:12:53 -0800

    Create Tooltip.tsx

---
+++ app/components/about-section/skills/Tooltip.tsx
@@ -0,0 +1,17 @@
+export default function Tooltip({
+}) {
+  return (
+    <>
+        <div
+        >
+          <div
+          />
+          <div
+          >
+            {skill.tech}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
