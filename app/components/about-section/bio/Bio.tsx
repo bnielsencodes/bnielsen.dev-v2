3c4a7f238db9f4b855c9d709f3a759dc5bb50975
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-18 15:34:45 -0800

    Fix error

--- app/components/about-section/bio/Bio.tsx
+++ app/components/about-section/bio/Bio.tsx
@@ -1,4 +1,5 @@
 import Avatar from "./Avatar";
+export default function Bio(props: any) {
   return (
     <article className="flex flex-col items-center gap-16 mx-auto mb-20">
       <p className=" mt-16 text-neutral-500 ">
