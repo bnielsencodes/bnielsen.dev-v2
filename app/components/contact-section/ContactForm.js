50292600d64634ec404ec2f2f08f2043f670a1c5
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-19 05:33:03 -0800

    Add inputs

--- app/components/contact-section/ContactForm.js
+++ app/components/contact-section/ContactForm.js
@@ -8,6 +8,29 @@ export default function ContactForm({ darkMode }) {
       className={`w-full mt-[100px] mx-auto ${styles.form}`}
       onSubmit={handleSubmit}
     >
+      <fieldset className="flex flex-col items-center pt-[0.35rem] pb-[10px] border-none ">
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          aria-label="Enter your name"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          aria-label="Enter your email address"
+          required
+        />
+        <textarea
+          name="message"
+          placeholder="Message"
+          aria-label="Enter a message"
+          required
+          rows="7"
+        />
+      </fieldset>
     </form>
   );
 }
