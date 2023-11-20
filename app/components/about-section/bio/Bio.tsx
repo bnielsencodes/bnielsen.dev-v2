921c8dfb7353f3a1b2a12d1292e91442b0a9e914
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-19 22:13:15 -0800

    Update spacing
    Also add placeholder text

--- app/components/about-section/bio/Bio.tsx
+++ app/components/about-section/bio/Bio.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 
 export default function Bio({ darkMode }: { darkMode: boolean }) {
   return (
-    <article className="flex flex-col items-center gap-16 mx-auto mb-20">
+    <article className="flex flex-col items-center gap-8 mx-auto mb-24">
       <Avatar />
       <p
         className={clsx("", {
@@ -11,6 +11,13 @@ export default function Bio({ darkMode }: { darkMode: boolean }) {
           "text-neutral-200": !darkMode,
         })}
       >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
       </p>
     </article>
   );
