396886aca1fb06c301012dda04774b9b8e9d5494
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-20 01:29:51 -0800

    Update section heading

--- app/components/contact-section/ContactSection.tsx
+++ app/components/contact-section/ContactSection.tsx
@@ -13,16 +13,16 @@ export default function ContactSection({ darkMode }: { darkMode: boolean }) {
       {/* section heading */}
       <p
         className={clsx(
-          "relative z-0 w-[176px] mx-auto mb-[10px] text-[2.5rem] font-bold text-center after:absolute after:content-[''] after:-z-10 after:right-0 after:bottom-[5px] after:w-[127px] after:h-[14px]",
+          "relative z-0 w-full mx-auto mb-[10px] text-[1.8rem] font-bold text-center after:absolute after:content-[''] after:-z-10 after:right-0 after:bottom-[5px] after:w-[281px] after:h-[10px]",
           {
             "text-neutral-500 after:bg-neutral-300": darkMode,
             "text-neutral-200 after:bg-neutral-400": !darkMode,
           }
         )}
       >
-        Contact
+        Want to work together?
       </p>
-      <ContactForm {...{darkMode}} />
+      <ContactForm {...{ darkMode }} />
     </section>
   );
 }
