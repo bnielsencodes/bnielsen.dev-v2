7fa4462210cd08fccaee780ca9214207d25f3b77
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-20 01:37:23 -0800

    Add header to form

--- app/components/contact-section/ContactForm.js
+++ app/components/contact-section/ContactForm.js
@@ -1,11 +1,66 @@
 import { useForm, ValidationError } from "@formspree/react";
+import Image from "next/image";
+import arrowIconDark from "@/public/assets/arrow/arrow-right-dark.svg";
+import arrowIconLight from "@/public/assets/arrow/arrow-right-light.svg";
+import clsx from "clsx";
 
 export default function ContactForm({ darkMode }) {
   const [state, handleSubmit] = useForm("mknewaqj");
 
   return (
-    <form className="w-full mt-[100px] mx-auto" onSubmit={handleSubmit}>
-      <fieldset className="flex flex-col items-center pt-[0.35rem] pb-[10px] border-none">
+    <form className="w-full max-w-[475px] mt-4 mx-auto" onSubmit={handleSubmit}>
+      <h2
+        className={clsx("m-0 text-2xl font-bold text-center", {
+          "text-neutral-500": darkMode,
+          "text-neutral-200": !darkMode,
+        })}
+      >
+        Let&apos;s connect!
+      </h2>
+      <div
+        className={clsx("pt-[6px] mb-2 font-sans text-[0.92rem] text-center", {
+          "text-neutral-400": darkMode,
+          "text-neutral-300": !darkMode,
+        })}
+      >
+        Email me at{" "}
+        {darkMode ? (
+          <Image
+            className="inline w-[12px] h-[12px] ml-[1px] mr-[3px] mb-[2px]"
+            src={arrowIconDark}
+            alt="arrow pointing right"
+            width="0"
+            height="0"
+            sizes="100vw"
+          />
+        ) : !darkMode ? (
+          <Image
+            className="inline w-[12px] h-[12px] ml-[1px] mr-[3px] mb-[2px]"
+            src={arrowIconLight}
+            alt="arrow pointing right"
+            width="0"
+            height="0"
+            sizes="100vw"
+          />
+        ) : null}
+        <strong>
+          <a
+            className={clsx(
+              "inline p-0 border-0 text-accent hover:opacity-90",
+              {
+                "bg-neutral-200": darkMode,
+                "bg-neutral-500": !darkMode,
+              }
+            )}
+            href="mailto:bnielsencodes@gmail.com"
+          >
+            brandon@bnielsen.dev
+          </a>
+        </strong>
+        <br />
+        or send me a quick message here:
+      </div>
+
         <input
           className={
             ("w-full h-[45px] pl-[10px] border-[1px] rounded-lg mb-[6px] font-sans caret-accent-100 focus:outline-2 focus:outline-accent-100",
