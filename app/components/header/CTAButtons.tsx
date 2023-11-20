c0dde21f436e4664efeb1c14bd42ada017dbc10e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-20 01:28:53 -0800

    Update styles

--- app/components/header/CTAButtons.tsx
+++ app/components/header/CTAButtons.tsx
@@ -14,12 +14,15 @@ export default function CTAButtons({ darkMode }: { darkMode: boolean }) {
         <a href="#projects-section">View Work</a>
       </button>
       <button
-        className={clsx("py-[10px] px-5 border rounded-lg text-lg font-bold", {
-          "border-neutral-300 bg-gradient-to-b from-neutral-200 to-neutral-100 text-neutral-500":
-            darkMode,
-          "shadow-custom border-neutral-300 bg-gradient-to-b from-neutral-500 to-neutral-600 text-neutral-200":
-            !darkMode,
-        })}
+        className={clsx(
+          "py-[10px] px-5 border border-neutral-300 rounded-lg text-lg font-bold",
+          {
+            "bg-gradient-to-b from-neutral-200 to-neutral-100 text-neutral-500":
+              darkMode,
+            "shadow-custom bg-gradient-to-b from-neutral-500 to-neutral-600 text-neutral-200":
+              !darkMode,
+          }
+        )}
       >
         <a href="#contact-section">Contact</a>
       </button>
