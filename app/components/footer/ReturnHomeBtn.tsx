aa94697139152a4e5784f417ed42ad9fd913d6ec
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-21 00:12:33 -0800

    Update styles

--- app/components/footer/ReturnHomeBtn.tsx
+++ app/components/footer/ReturnHomeBtn.tsx
@@ -7,7 +7,7 @@ export default function ReturnHomeBtn({ darkMode }: { darkMode: boolean }) {
     <>
       <button
         className={clsx(
-          "absolute -top-[20px] w-10 h-10 border-[1px] border-neutral-300 rounded-lg mx-auto bg-opacity-80 backdrop-blur-sm hvr-bob hover:bg-accent hover:scale-[1.1]",
+          "accent-outline absolute -top-[20px] w-10 h-10 border-[1px] border-neutral-300 rounded-lg mx-auto bg-opacity-80 backdrop-blur-sm hvr-bob hover:scale-[1.1]",
           {
             "bg-gradient-to-b from-neutral-200 to-neutral-100 lg:hover:bg-gradient-to-b lg:hover:from-neutral-200 lg:hover:to-neutral-200":
               darkMode,
@@ -18,7 +18,7 @@ export default function ReturnHomeBtn({ darkMode }: { darkMode: boolean }) {
         title="Return to top of page"
       >
         <a
-          className="flex items-center justify-center w-full h-full focus:outline-2 focus:text-warning"
+          className="flex items-center justify-center w-full h-full"
           href="#header"
           aria-label="return home"
         >
