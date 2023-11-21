a6fb2ec3b7d282ff56c4cd0452e35568201a7bf8
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-21 01:24:11 -0800

    Update header text and cta buttons animations

--- app/components/header/CTAButtons.tsx
+++ app/components/header/CTAButtons.tsx
@@ -4,7 +4,7 @@ export default function CTAButtons({ darkMode }: { darkMode: boolean }) {
   return (
     <div className="flex gap-4 mt-[21px]">
       <button
-        className={clsx("btn", {
+        className={clsx("btn header-cta--left", {
           "btn-light": darkMode,
           "btn-dark shadow-custom": !darkMode,
         })}
@@ -12,10 +12,13 @@ export default function CTAButtons({ darkMode }: { darkMode: boolean }) {
         <a href="#projects-section">View Work</a>
       </button>
       <button
-        className={clsx("btn border-[1px] border-neutral-300", {
-          "btn-dark": darkMode,
-          "btn-light shadow-custom": !darkMode,
-        })}
+        className={clsx(
+          "btn header-cta--right border-[1px] border-neutral-300",
+          {
+            "btn-dark": darkMode,
+            "btn-light shadow-custom": !darkMode,
+          }
+        )}
       >
         <a href="#contact-section">Contact</a>
       </button>
