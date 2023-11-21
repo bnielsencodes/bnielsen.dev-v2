d6d92455b3a33ab0a93cae23e8f5cb6c4d1d5131
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-20 19:15:33 -0800

    Update hamburger menu top bar size and update animation

--- app/components/navigation/HamburgerMenu.tsx
+++ app/components/navigation/HamburgerMenu.tsx
@@ -12,8 +12,9 @@ export default function HamburgerMenu({
   return (
     <button
       className={clsx(
-        "fixed top-2 right-2 z-50 flex flex-col justify-around items-center gap-1 w-[40px] h-[40px] py-3 px-2 group",
+        "fixed top-2 right-2 z-50 flex flex-col justify-around items-end gap-1 w-[40px] h-[40px] py-3 px-2 group",
         {
+          "items-center": showMobileNav,
           "border-2 border-neutral-500": showMobileNav && darkMode,
           "border-2 border-neutral-200": showMobileNav && !darkMode,
         }
@@ -22,9 +23,10 @@ export default function HamburgerMenu({
     >
       <span
         className={clsx(
-          "inline-block w-5 h-[2px] transition-transform duration-400",
+          "inline-block h-[2px] transition-transform duration-500",
           {
-            "rotate-45 translate-y-1": showMobileNav,
+            "w-6 -rotate-[135deg] translate-y-1": showMobileNav,
+            "w-7": !showMobileNav,
             "bg-neutral-400": showMobileNav && darkMode,
             "bg-neutral-300": showMobileNav && !darkMode,
             "bg-neutral-600": !showMobileNav && darkMode,
@@ -34,9 +36,9 @@ export default function HamburgerMenu({
       ></span>
       <span
         className={clsx(
-          "inline-block w-5 h-[2px] transition-transform duration-400",
+          "inline-block w-6 h-[2px] transition-transform duration-500",
           {
-            "-rotate-45 -translate-y-1": showMobileNav,
+            "rotate-[135deg] -translate-y-1": showMobileNav,
             "bg-neutral-400": showMobileNav && darkMode,
             "bg-neutral-300": showMobileNav && !darkMode,
             "bg-neutral-600": !showMobileNav && darkMode,
