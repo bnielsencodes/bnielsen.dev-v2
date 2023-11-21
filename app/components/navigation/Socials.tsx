9a17d3432b0bd4b808c95204312ba00c708fef11
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-20 20:53:38 -0800

    Remove img hover effects

--- app/components/navigation/Socials.tsx
+++ app/components/navigation/Socials.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
 import iconLinkedInNeutral200 from "/public/assets/socials/linkedin/mobile/linkedin-neutral-200.svg";
 import iconLinkedInNeutral400 from "/public/assets/socials/linkedin/mobile/linkedin-neutral-400.svg";
-import iconLinkedInAccent from "/public/assets/socials/linkedin/mobile/linkedin-accent.svg";
 import iconGitHubNeutral200 from "/public/assets/socials/github/mobile/github-neutral-200.svg";
 import iconGitHubNeutral400 from "/public/assets/socials/github/mobile/github-neutral-400.svg";
-import iconGitHubAccent from "/public/assets/socials/github/mobile/github-accent.svg";
 import iconEmailNeutral200 from "/public/assets/socials/email/mobile/email-neutral-200.svg";
 import iconEmailNeutral400 from "/public/assets/socials/email/mobile/email-neutral-400.svg";
-import iconEmailAccent from "/public/assets/socials/email/mobile/email-accent.svg";
-import clsx from "clsx";
 
 export default function Socials({ darkMode }: { darkMode: boolean }) {
   return (
@@ -19,7 +15,6 @@ export default function Socials({ darkMode }: { darkMode: boolean }) {
           <a className="group" href="https://linkedin.com/in/bnielsencodes">
             {darkMode ? (
               <Image
-                className="w-10 h-10 mr-2 group-hover:hidden"
                 src={iconLinkedInNeutral400}
                 alt="LinkedIn icon"
                 width="0"
@@ -28,7 +23,6 @@ export default function Socials({ darkMode }: { darkMode: boolean }) {
               />
             ) : (
               <Image
-                className="w-10 h-10 mr-2 group-hover:hidden"
                 src={iconLinkedInNeutral200}
                 alt="LinkedIn icon"
                 width="0"
@@ -36,15 +30,6 @@ export default function Socials({ darkMode }: { darkMode: boolean }) {
                 sizes="100vw"
               />
             )}
-            {/* images shown on hover */}
-            <Image
-              className="w-10 h-10 mr-2 hidden group-hover:block"
-              src={iconLinkedInAccent}
-              alt="LinkedIn icon"
-              width="0"
-              height="0"
-              sizes="100vw"
-            />
           </a>
         </li>
 
@@ -53,7 +38,6 @@ export default function Socials({ darkMode }: { darkMode: boolean }) {
           <a className="group" href="https://github.com/bnielsencodes">
             {darkMode ? (
               <Image
-                className="w-10 h-10 mr-2 group-hover:hidden"
                 src={iconGitHubNeutral400}
                 alt="GitHub icon"
                 width="0"
@@ -62,7 +46,6 @@ export default function Socials({ darkMode }: { darkMode: boolean }) {
               />
             ) : (
               <Image
-                className="w-10 h-10 mr-2 group-hover:hidden"
                 src={iconGitHubNeutral200}
                 alt="GitHub icon"
                 width="0"
@@ -70,15 +53,6 @@ export default function Socials({ darkMode }: { darkMode: boolean }) {
                 sizes="100vw"
               />
             )}
-            {/* images shown on hover */}
-            <Image
-              className="w-10 h-10 mr-2 hidden group-hover:block"
-              src={iconGitHubAccent}
-              alt="GitHub icon"
-              width="0"
-              height="0"
-              sizes="100vw"
-            />
           </a>
         </li>
 
@@ -87,7 +61,6 @@ export default function Socials({ darkMode }: { darkMode: boolean }) {
           <a className="group" href="mailto:brandon@bnielsen.dev">
             {darkMode ? (
               <Image
-                className="w-10 h-10 group-hover:hidden"
                 src={iconEmailNeutral400}
                 alt="GitHub icon"
                 width="0"
@@ -96,7 +69,6 @@ export default function Socials({ darkMode }: { darkMode: boolean }) {
               />
             ) : (
               <Image
-                className="w-10 h-10 group-hover:hidden"
                 src={iconEmailNeutral200}
                 alt="GitHub icon"
                 width="0"
@@ -104,15 +76,6 @@ export default function Socials({ darkMode }: { darkMode: boolean }) {
                 sizes="100vw"
               />
             )}
-            {/* images shown on hover */}
-            <Image
-              className="w-10 h-10 hidden group-hover:block"
-              src={iconEmailAccent}
-              alt="GitHub icon"
-              width="0"
-              height="0"
-              sizes="100vw"
-            />
           </a>
         </li>
       </ul>
