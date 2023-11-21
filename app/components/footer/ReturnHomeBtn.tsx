005babbcb90da9974097b799fdb7aba27484c125
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-21 10:08:21 -0800

    Clean up code

--- app/components/footer/ReturnHomeBtn.tsx
+++ app/components/footer/ReturnHomeBtn.tsx
@@ -7,11 +7,11 @@ export default function ReturnHomeBtn({ darkMode }: { darkMode: boolean }) {
     <>
       <button
         className={clsx(
-          "accent-outline absolute -top-[20px] w-10 h-10 border-[1px] border-neutral-300 rounded-lg mx-auto bg-opacity-80 backdrop-blur-sm hvr-bob hover:scale-[1.1]",
+          "hvr-bob accent-outline absolute -top-[20px] w-10 h-10 border-[1px] border-neutral-300 rounded-lg mx-auto bg-opacity-80 backdrop-blur-sm",
           {
-            "bg-gradient-to-b from-neutral-200 to-neutral-100 lg:hover:bg-gradient-to-b lg:hover:from-neutral-200 lg:hover:to-neutral-200":
+            "bg-gradient-to-b from-neutral-200 to-neutral-100 lg:hover:to-neutral-200":
               darkMode,
-            "shadow-custom bg-gradient-to-b from-neutral-500 to-neutral-600  lg:hover:bg-gradient-to-b lg:hover:from-neutral-500 lg:hover:to-neutral-500":
+            "shadow-custom bg-gradient-to-b from-neutral-500 to-neutral-600 lg:hover:to-neutral-500":
               !darkMode,
           }
         )}
@@ -21,7 +21,7 @@ export default function ReturnHomeBtn({ darkMode }: { darkMode: boolean }) {
         <a
           className="flex items-center justify-center w-full h-full"
           href="#header"
-          aria-label="return home"
+          aria-label="return to top of page"
         >
           <Image
             className="w-7 h-7"
