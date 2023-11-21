bc8115d26e973e6a1c9a95cbfdbd01156dc1cf2a
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-21 00:16:10 -0800

    Add comments

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,16 @@ import clsx from "clsx";
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
 
+  //
+  //
+
   // toggle dark mode
   function toggleTheme() {
     setDarkMode((prevMode) => !prevMode);
   }
+
+  //
+  //
 
   return (
     <>
