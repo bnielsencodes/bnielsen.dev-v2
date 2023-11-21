aaf7d4c6f0a2a4210b2f4d9cc92e9299d16df549
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-21 01:43:42 -0800

    Update header animations using Framer Motion

--- app/components/header/CTAButtons.tsx
+++ app/components/header/CTAButtons.tsx
@@ -1,27 +1,45 @@
+import { motion } from "framer-motion";
 import clsx from "clsx";
 
 export default function CTAButtons({ darkMode }: { darkMode: boolean }) {
   return (
     <div className="flex gap-4 mt-[21px]">
-      <button
-        className={clsx("btn header-cta--left", {
-          "btn-light": darkMode,
-          "btn-dark shadow-custom": !darkMode,
-        })}
+      <motion.div
+        initial={{ opacity: 0, translateX: "-10rem" }}
+        whileInView={{ opacity: 1, translateX: "0" }}
+        transition={{
+          duration: 1.4,
+          ease: [0.21, 1.11, 0.81, 0.99],
+        }}
+        viewport={{ once: true }}
       >
-        <a href="#projects-section">View Work</a>
-      </button>
-      <button
-        className={clsx(
-          "btn header-cta--right border-[1px] border-neutral-300",
-          {
+        <button
+          className={clsx("btn", {
+            "btn-light": darkMode,
+            "btn-dark shadow-custom": !darkMode,
+          })}
+        >
+          <a href="#projects-section">View Work</a>
+        </button>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, translateX: "10rem" }}
+        whileInView={{ opacity: 1, translateX: "0" }}
+        transition={{
+          duration: 1.4,
+          ease: [0.21, 1.11, 0.81, 0.99],
+        }}
+        viewport={{ once: true }}
+      >
+        <button
+          className={clsx("btn border-[1px] border-neutral-300", {
             "btn-dark": darkMode,
             "btn-light shadow-custom": !darkMode,
-          }
-        )}
-      >
-        <a href="#contact-section">Contact</a>
-      </button>
+          })}
+        >
+          <a href="#contact-section">Contact</a>
+        </button>
+      </motion.div>
     </div>
   );
 }
--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import CTAButtons from "./CTAButtons";
 import clsx from "clsx";
 
@@ -5,28 +6,39 @@ export default function Header({ darkMode }: { darkMode: boolean }) {
   return (
     <header
       id="header"
-      className="flex flex-col items-center justify-center h-screen px-7"
+      className="flex flex-col items-center justify-center h-screen px-7 mb-[6px]"
     >
-      <h1
-        className={clsx(
-          "header-text mt-3 mb-[6px] text-[2.48rem] font-bold leading-[3rem] tracking-wide text-left",
-          {
-            "text-neutral-600": darkMode,
-            "text-neutral-100": !darkMode,
-          }
-        )}
+      <motion.div
+        initial={{ opacity: 0, translateY: "-10rem" }}
+        whileInView={{ opacity: 1, translateY: "0" }}
+        transition={{
+          duration: 1.4,
+          ease: [0.21, 1.11, 0.81, 0.99],
+        }}
+        viewport={{ once: true }}
       >
-        Hello, I&apos;m Brandon Nielsen, a{" "}
-        <span
-          className={clsx("", {
-            "header-text-stroke--dark text-neutral-100": darkMode,
-            "header-text-stroke text-neutral-600": !darkMode,
-          })}
+        <h1
+          className={clsx(
+            "text-[2.48rem] font-bold leading-[3rem] tracking-wide text-left",
+            {
+              "text-neutral-600": darkMode,
+              "text-neutral-100": !darkMode,
+            }
+          )}
         >
-          Web Developer
-        </span>
-        .
-      </h1>
+          Hello, I&apos;m Brandon Nielsen, a{" "}
+          <span
+            className={clsx("", {
+              "header-text-stroke--dark text-neutral-100": darkMode,
+              "header-text-stroke text-neutral-600": !darkMode,
+            })}
+          >
+            Web Developer
+          </span>
+          .
+        </h1>
+      </motion.div>
+
       <CTAButtons {...{ darkMode }} />
     </header>
   );
