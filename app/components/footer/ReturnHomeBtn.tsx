f6b5e24857eb00927dac87552d1445a177eb1ae3
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-22 11:16:46 -0800

    Update styles

--- app/components/footer/ReturnHomeBtn.tsx
+++ app/components/footer/ReturnHomeBtn.tsx
@@ -5,12 +5,13 @@ import clsx from "clsx";
 export default function ReturnHomeBtn({ darkMode }: { darkMode: boolean }) {
   return (
     <a
+      className="hvr-bob accent-outline absolute -top-[20px]"
       href="#header"
       aria-label="return to top of page"
     >
       <button
         className={clsx(
-          "hvr-bob accent-outline absolute -top-[20px] flex items-center justify-center w-10 h-10 border-[1px] border-neutral-300 rounded-lg mx-auto bg-opacity-80 backdrop-blur-sm",
+          "flex items-center justify-center w-10 h-10 border-[1px] border-neutral-300 rounded-lg mx-auto bg-opacity-80 backdrop-blur-sm",
           {
             "bg-gradient-to-b from-neutral-200 to-neutral-100 lg:hover:to-neutral-200":
               darkMode,
