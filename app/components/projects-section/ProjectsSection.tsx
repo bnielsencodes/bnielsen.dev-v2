657a97b947fcc00c0850d5f18b1a203a7c166404
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-21 23:12:32 -0800

    Make more semantic

--- app/components/projects-section/ProjectsSection.tsx
+++ app/components/projects-section/ProjectsSection.tsx
@@ -16,7 +16,7 @@ export default function ProjectsSection({ darkMode }: { darkMode: boolean }) {
       })}
     >
       {/* section heading */}
-      <p
+      <h2
         className={clsx(
           "relative z-0 w-[130px] mx-auto text-[2.5rem] font-bold text-center after:absolute after:content-[''] after:-z-10 after:right-0 after:bottom-[5px] after:w-[93px] after:h-[14px]",
           {
@@ -26,7 +26,7 @@ export default function ProjectsSection({ darkMode }: { darkMode: boolean }) {
         )}
       >
         Work
-      </p>
+      </h2>
       <div className="flex flex-col justify-center pb-[25px] mt-[120px] text-[0.9rem] ">
         {projectElements}
       </div>
