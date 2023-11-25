8211273e52df62a66b43c438f79d9365d844261a
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-25 05:08:34 -0800

    Reformat

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -6,24 +6,22 @@ const isFooter = true;
 
 export default function Footer({ darkMode }: { darkMode: boolean }) {
   return (
-    <>
-      <section
-        className={clsx(
-          "relative flex flex-col items-center justify-center h-40 pt-[25px]",
-          { "bg-neutral-100": darkMode, "bg-neutral-600": !darkMode }
-        )}
+    <footer
+      className={clsx(
+        "relative flex flex-col items-center justify-center h-40 pt-[25px]",
+        { "bg-neutral-100": darkMode, "bg-neutral-600": !darkMode }
+      )}
+    >
+      <ReturnHomeBtn {...{ darkMode }} />
+      <Socials {...{ darkMode, isFooter }} />
+      <p
+        className={clsx("mt-[18px] font-sans text-[0.725rem] uppercase", {
+          "text-neutral-400": darkMode,
+          "text-neutral-300": !darkMode,
+        })}
       >
-        <ReturnHomeBtn {...{ darkMode }} />
-        <Socials {...{ darkMode, isFooter }} />
-        <p
-          className={clsx("mt-[18px] font-sans text-[0.725rem] uppercase", {
-            "text-neutral-400": darkMode,
-            "text-neutral-300": !darkMode,
-          })}
-        >
-          Brandon Nielsen - 2023
-        </p>
-      </section>
-    </>
+        Brandon Nielsen - 2023
+      </p>
+    </footer>
   );
 }
