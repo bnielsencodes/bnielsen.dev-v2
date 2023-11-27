965a2dba9f3b981a4e5e344b05a714ee129c869d
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-27 11:41:30 -0800

    Reformat

--- app/components/contact-section/ClipboardCopy.tsx
+++ app/components/contact-section/ClipboardCopy.tsx
@@ -33,23 +33,19 @@ export default function ClipboardCopy({ darkMode }: { darkMode: boolean }) {
   return (
     <>
       <button
-        className={clsx("p-0 border-0 hover:cursor-pointer hover:opacity-[0.8] focus:outline-2", { 
-            "focus:outline-accent-200": darkMode,
-            "focus:outline-accent-100": !darkMode
+        className={clsx("p-0 border-0 hover:cursor-pointer hover:opacity-80 focus:outline-2 focus:opacity-80", { 
+            "text-accent-200 focus:outline-accent-200": darkMode,
+            "text-accent-100 focus:outline-accent-100": !darkMode
         })}
         onClick={handleCopyClick}
         title="Copy to clipboard"
       >
-        <strong
-          className={clsx("", {
-            "text-accent-200": darkMode,
-            "text-accent-100": !darkMode,
-          })}
-        >
+        <strong>
           brandon@bnielsen.dev
         </strong>
       </button>
-      <span className="absolute -top-[10px] -right-[70px] p-[0.5rem] rounded-[0.2rem] ml-4 bg-none text-[13px] tracking-[0.4px] uppercase ">
+      
+      <span className="absolute -top-[10px] -right-[70px] p-[0.5rem] rounded-[0.2rem] ml-4 bg-none text-[13px] tracking-[0.4px] uppercase">
         {isCopied ? "Copied!" : ""}
       </span>
     </>
