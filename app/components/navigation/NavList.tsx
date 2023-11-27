bc3baaff569aefeeafa232974fd8d9b88be34278
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-11-26 22:57:54 -0800

    Create NavList.tsx

---
+++ app/components/navigation/NavList.tsx
@@ -0,0 +1,55 @@
+import clsx from "clsx";
+
+export default function NavList({ darkMode }: { darkMode: boolean }) {
+  return (
+    <>
+      <ul className="lg:flex lg:justify-between">
+        <li className="nav-item">
+          <a
+            className={clsx(
+              "nav-link underlineAnimation focus:outline-2 focus:opacity-70",
+              {
+                "text-neutral-500 focus:outline-accent-200": darkMode,
+                "text-neutral-200 focus:outline-accent-100": !darkMode,
+              }
+            )}
+            href="#about-section"
+            aria-label="about"
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className={clsx(
+              "nav-link underlineAnimation focus:outline-2 focus:opacity-70",
+              {
+                "text-neutral-500 outline-2 outline-accent-200": darkMode,
+                "text-neutral-200 outline-accent-100": !darkMode,
+              }
+            )}
+            href="#projects-section"
+            aria-label="work"
+          >
+            Work
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            className={clsx(
+              "nav-link underlineAnimation focus:outline-2 focus:opacity-70",
+              {
+                "text-neutral-500 outline-2 outline-accent-200": darkMode,
+                "text-neutral-200 outline-accent-100": !darkMode,
+              }
+            )}
+            href="#contact-section"
+            aria-label="contact"
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+}
