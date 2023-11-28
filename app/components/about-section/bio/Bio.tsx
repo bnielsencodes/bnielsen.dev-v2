849bed0859a14efe030ed4c53279350e6a22bb1e
Brandon Nielsen <bnielsencodes@gmail.com>
2023-11-28 15:05:16 -0800

    Update Bio.tsx

--- app/components/about-section/bio/Bio.tsx
+++ app/components/about-section/bio/Bio.tsx
@@ -12,11 +12,11 @@ export default function Bio({ darkMode }: { darkMode: boolean }) {
           "text-neutral-200": !darkMode,
         })}
       >
-        <p>My journey in the world of web development has equipped me with a unique set of skills, enabling me to combine analytical thinking with a keen eye for aesthetics. Whether it&apos;s designing an intuitive user interface, optimizing website performance, or tackling complex coding challenges, I always strive for excellence.</p>
+        <p className="mb-4">My journey in the world of web development has equipped me with a unique set of skills, enabling me to combine analytical thinking with a keen eye for aesthetics. Whether it&apos;s designing an intuitive user interface, optimizing website performance, or tackling complex coding challenges, I always strive for excellence.</p>
 
-        <p>With a firm belief in the power of innovation and continuous improvement, I take on every project as an opportunity to push boundaries and exceed expectations. By keeping myself updated with the latest industry trends and emerging technologies, I ensure that my work remains at the forefront of the ever-evolving web development landscape.</p>
+        <p className="mb-4">With a firm belief in the power of innovation and continuous improvement, I take on every project as an opportunity to push boundaries and exceed expectations. By keeping myself updated with the latest industry trends and emerging technologies, I ensure that my work remains at the forefront of the ever-evolving web development landscape.</p>
 
-        <p>From conceptualization to implementation, I invest myself wholeheartedly in each project, driven by the desire to provide the best user experience. By combining my technical expertise with an artistic touch, I aim to leave a lasting impression on every website I create.</p>
+        <p className="mb-4">From conceptualization to implementation, I invest myself wholeheartedly in each project, driven by the desire to provide the best user experience. By combining my technical expertise with an artistic touch, I aim to leave a lasting impression on every website I create.</p>
         
         <p>In a constantly changing digital world, I am excited to embrace new challenges, learn from experiences, and continue growing as a web developer. Through my work, I hope to inspire and elevate online experiences for users across the globe.</p>
       </div>
