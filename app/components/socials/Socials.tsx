bbf2048074926902ba1eac0a05dd16523fead51f
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-02 04:31:10 -0800

    Pass props

--- app/components/socials/Socials.tsx
+++ app/components/socials/Socials.tsx
@@ -1,14 +1,28 @@
-import Social from "./Social";
 import { socials } from "@/app/data";
+import Social from "./Social";
+import clsx from "clsx";
 
-export default function Socials({ darkMode }: { darkMode: boolean }) {
+export default function Socials({
+  darkMode,
+  parent,
+  direction,
+}: {
+  darkMode: boolean;
+  parent: string;
+  direction: string;
+}) {
   const socialElements = socials.map((social) => {
-    return <Social key={social.id} {...{ darkMode, social }} />;
+    return <Social key={social.id} parent={parent} {...{ darkMode, social }} />;
   });
 
   return (
-    <>
-      <ul className="flex gap-4 lg:flex-col">{socialElements}</ul>
-    </>
+    <ul
+      className={clsx("flex gap-4", {
+        "flex-col": direction === "column",
+        "flex-row": direction === "row",
+      })}
+    >
+      {socialElements}
+    </ul>
   );
 }
