320a410e478cc068a7d554cd7b893f1cadccdf5c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-02 22:10:36 -0800

    Update spacing

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header({ darkMode }: { darkMode: boolean }) {
   return (
     <header
       id="header"
-      className="relative flex flex-col items-center justify-center h-screen pt-[56px] px-[26px] mb-[6px] lg:items-start lg:pt-0 lg:pl-[31px] xl:items-center"
+      className="relative flex flex-col items-center justify-center h-screen pt-[56px] px-[26px] mb-[6px] lg:items-start lg:pl-[31px] xl:items-center"
     >
       {/* Avatar image on larger screens */}
       <Avatar parent="header" />
@@ -15,7 +15,7 @@ export default function Header({ darkMode }: { darkMode: boolean }) {
       {/* header intro */}
       <h1
         className={clsx(
-          "w-[323px] mt-5 mb-6 text-[2.414rem] font-extrabold leading-[2.8rem] tracking-wide text-left md:w-[704px] md:text-[57px] md:leading-[3.85rem] lg:w-[830px] lg:text-[66px] lg:leading-[4.35rem] xl:w-[985px] xl:mt-[1.2rem] xl:text-[76px] xl:font-extrabold xl:leading-[5.35rem] xl:text-center",
+          "w-[323px] mt-5 mb-6 text-[2.414rem] font-extrabold leading-[2.8rem] tracking-wide text-left md:w-[704px] md:text-[57px] md:leading-[3.85rem] lg:w-[850px] lg:mt-[1.6rem] lg:text-[66px] lg:leading-[1.1em] xl:w-[985px] xl:text-[76px] xl:font-extrabold xl:leading-[5.35rem] xl:text-center",
           {
             "text-neutral-600": darkMode,
             "text-neutral-100": !darkMode,
