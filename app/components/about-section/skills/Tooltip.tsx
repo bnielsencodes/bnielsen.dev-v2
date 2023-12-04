871b0626f29aa207ae512f4498b3fd553bdb17c4
Brandon Nielsen <bnielsencodes@gmail.com>
2023-12-04 10:51:29 -0800

    Update tooltip positions for sm, md, and lg screens

--- app/components/about-section/skills/Tooltip.tsx
+++ app/components/about-section/skills/Tooltip.tsx
@@ -18,18 +18,18 @@ export default function Tooltip({
             "absolute -top-[15px] z-10 font-normal text-xs leading-[1.42857143] text-start",
             {
               "-top-[17px]": skill.name === "figma" || skill.name === "vite",
-              "left-[17px] md:left-[20px]":
-                skill.name === "figma" || skill.name === "next",
-              "left-[20px] md:left-[23px]":
+              "left-[20px] md:left-[23px] lg:left-[20px]":
                 skill.name === "html" || skill.name === "react",
-              "left-[25px] md:left-[28px]": skill.name === "css",
-              "left-[27px] md:left-[30px]": skill.name === "git",
-              "left-[10px] md:left-[13px]": skill.name === "javascript",
-              "left-[4px] md:left-[7px]": skill.name === "postgresql",
-              "left-[23px] md:left-[26px]": skill.name === "sass",
-              "left-[13px] md:left-[16px]": skill.name === "tailwind",
-              "left-[7px] md:left-[10px]": skill.name === "typescript",
-              "left-[22px] md:left-[25px]": skill.name === "vite",
+              "left-[25px] md:left-[28px] lg:left-[25px]": skill.name === "css",
+              "left-[20px] md:left-[23px]": skill.name === "figma",
+              "left-[27px] md:left-[32px] lg:left-[29px]": skill.name === "git",
+              "left-[10px] md:left-[13px] lg:left-[10px]": skill.name === "javascript",
+              "left-[17px] md:left-[20px] lg:left-[17px]": skill.name === "next",
+              "left-[4px] md:left-[7px] lg:left-[4px]": skill.name === "postgresql",
+              "left-[23px] md:left-[26px] lg:left-[23px]": skill.name === "sass",
+              "left-[13px] md:left-[16px] lg:left-[13px]": skill.name === "tailwind",
+              "left-[7px] md:left-[10px] lg:left-[7px]": skill.name === "typescript",
+              "left-[25px] md:left-[28px] lg:left-[25px]": skill.name === "vite",
             }
           )}
         >
