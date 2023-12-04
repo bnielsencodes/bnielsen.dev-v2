bbb5b6b9e4e04201bde2bbd9567ac42448a2d7b8
Brandon Nielsen <bnielsencodes@gmail.com>
2023-12-04 11:13:51 -0800

    Fix error

--- app/components/about-section/skills/Tooltip.tsx
+++ app/components/about-section/skills/Tooltip.tsx
@@ -19,9 +19,8 @@ export default function Tooltip({
             {
               "-top-[17px]": skill.name === "figma" || skill.name === "vite",
               "left-[20px] md:left-[23px] lg:left-[20px]":
-                skill.name === "html" || skill.name === "react",
+                skill.name === "figma" || skill.name === "html" || skill.name === "react",
               "left-[25px] md:left-[28px] lg:left-[25px]": skill.name === "css" || skill.name === "vite",
-              "left-[20px] md:left-[23px] lg:left-[20px]": skill.name === "figma",
               "left-[27px] md:left-[32px] lg:left-[28px]": skill.name === "git",
               "left-[10px] md:left-[13px] lg:left-[10px]": skill.name === "javascript",
               "left-[17px] md:left-[20px] lg:left-[17px]": skill.name === "next",
