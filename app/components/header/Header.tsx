b027f2568b6a79dd5ed6809a56ed9bb6e154620b
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-04 00:00:11 -0800

    Update header layout on lg screens

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header({ darkMode }: { darkMode: boolean }) {
   return (
     <header
       id="header"
-      className="relative flex flex-col items-start justify-center h-[100svh] pt-[56px] px-[17px] mb-[6px] md:px-[19px] lg:items-start lg:pl-[22px] xl:items-center"
+      className="relative flex flex-col items-start justify-center h-[100svh] pt-[56px] px-[17px] mb-[6px] md:px-[19px] lg:items-center lg:pl-[22px]"
     >
       {/* Avatar image on larger screens */}
       <Avatar parent="header" />
@@ -15,7 +15,7 @@ export default function Header({ darkMode }: { darkMode: boolean }) {
       {/* header intro */}
       <h1
         className={clsx(
-          "w-full mt-5 mb-5 text-[11vw] font-extrabold leading-[1.15em] tracking-wide text-left md:text-[7.6vw] lg:w-[850px] lg:mt-[1.6rem] lg:text-[66px] lg:leading-[1.1em] xl:w-[985px] xl:text-[76px] xl:font-extrabold xl:leading-[5.35rem] xl:text-center",
+          "w-full mt-5 mb-5 text-[11vw] font-extrabold leading-[1.15em] tracking-wide text-left md:text-[7.6vw] lg:w-[850px] lg:mt-[1.6rem] lg:text-[66px] lg:leading-[1.1em] lg:text-center xl:w-[985px] xl:text-[76px] xl:font-extrabold xl:leading-[5.35rem]",
           {
             "text-neutral-600": darkMode,
             "text-neutral-100": !darkMode,
