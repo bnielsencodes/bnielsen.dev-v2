2f1b892738debb04644787671973cf27c4c60386
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-03 20:48:00 -0800

    Add md screen animation

--- app/components/navigation/HamburgerMenu.tsx
+++ app/components/navigation/HamburgerMenu.tsx
@@ -12,11 +12,14 @@ export default function HamburgerMenu({
   return (
     <button
       className={clsx(
-        "fixed top-2 right-2 z-50 flex flex-col justify-around items-end gap-1 w-[40px] h-[40px] py-3 px-2 group lg:hidden",
+        "fixed top-2 right-2 z-50 flex flex-col justify-around items-end gap-1 w-[40px] h-[40px] py-3 px-2 md:right-0 md:-translate-x-4 group lg:hidden",
         {
-          "items-center": showMobileNav,
-          "outline-dark-theme border-2 border-neutral-500": showMobileNav && darkMode,
-          "outline-light-theme border-2 border-neutral-200": showMobileNav && !darkMode,
+          "items-center md:-translate-x-[8px] md:transition-transform md:duration-500":
+            showMobileNav,
+          "outline-dark-theme border-2 border-neutral-500":
+            showMobileNav && darkMode,
+          "outline-light-theme border-2 border-neutral-200":
+            showMobileNav && !darkMode,
         }
       )}
       onClick={() => toggleMobileNav()}
