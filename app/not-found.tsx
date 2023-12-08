d5e27267c5f6f3c35ed4388481943131e79f0973
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-08 11:34:25 -0800

    Update spacing

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -25,7 +25,7 @@ export default function NotFound() {
     >
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center w-full text-2xl font-extrabold">
         <h1 className="text-7xl">404</h1>
-        <h2 className="mt-2 mb-6 text-4xl">Page Not Found</h2>
+        <h2 className="mt-4 mb-6 text-4xl">Page Not Found</h2>
         <button
           className={clsx("btn btn-primary", {
             "btn-light": darkMode,
