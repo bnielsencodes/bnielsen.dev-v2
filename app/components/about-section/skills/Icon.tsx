c8ab33d3efe660280712f2534b99df9b5e696d7e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-12-16 21:52:08 -0800

    Update Figma icon hover stroke effect

--- app/components/about-section/skills/Icon.tsx
+++ app/components/about-section/skills/Icon.tsx
@@ -115,31 +115,31 @@ export default function CSS({
             viewBox="0 0 128 128"
           >
             <path
-              className="fill-transition group-hover:fill-tech-figma-100 group-hover:stroke-tech-figma-100"
+              className="fill-transition group-hover:fill-tech-figma-100"
               fill="#ededed"
               stroke="#1a1a1a"
               d="M45.5 129c11.9 0 21.5-9.6 21.5-21.5V86H45.5C33.6 86 24 95.6 24 107.5S33.6 129 45.5 129zm0 0"
             ></path>
             <path
-              className="fill-transition group-hover:fill-tech-figma-200 group-hover:stroke-tech-figma-200"
+              className="fill-transition group-hover:fill-tech-figma-200"
               fill="#ededed"
               stroke="#1a1a1a"
               d="M24 64.5C24 52.6 33.6 43 45.5 43H67v43H45.5C33.6 86 24 76.4 24 64.5zm0 0"
             ></path>
             <path
-              className="fill-transition group-hover:fill-tech-figma-300 group-hover:stroke-tech-figma-300"
+              className="fill-transition group-hover:fill-tech-figma-300"
               fill="#ededed"
               stroke="#1a1a1a"
               d="M24 21.5C24 9.6 33.6 0 45.5 0H67v43H45.5C33.6 43 24 33.4 24 21.5zm0 0"
             ></path>
             <path
-              className="fill-transition group-hover:fill-tech-figma-400 group-hover:stroke-tech-figma-400"
+              className="fill-transition group-hover:fill-tech-figma-400"
               fill="#ededed"
               stroke="#1a1a1a"
               d="M67 0h21.5C100.4 0 110 9.6 110 21.5S100.4 43 88.5 43H67zm0 0"
             ></path>
             <path
-              className="fill-transition group-hover:fill-tech-figma-500 group-hover:stroke-tech-figma-500"
+              className="fill-transition group-hover:fill-tech-figma-500"
               fill="#fafafa"
               stroke="#1a1a1a"
               d="M110 64.5c0 11.9-9.6 21.5-21.5 21.5S67 76.4 67 64.5 76.6 43 88.5 43 110 52.6 110 64.5zm0 0"
@@ -154,31 +154,31 @@ export default function CSS({
             viewBox="0 0 128 128"
           >
             <path
-              className="fill-transition group-hover:fill-tech-figma-100 group-hover:stroke-tech-figma-100"
+              className="fill-transition group-hover:fill-tech-figma-100"
               fill="#272727"
               stroke="#fafafa"
               d="M45.5 129c11.9 0 21.5-9.6 21.5-21.5V86H45.5C33.6 86 24 95.6 24 107.5S33.6 129 45.5 129zm0 0"
             ></path>
             <path
-              className="fill-transition group-hover:fill-tech-figma-200 group-hover:stroke-tech-figma-200"
+              className="fill-transition group-hover:fill-tech-figma-200"
               fill="#272727"
               stroke="#fafafa"
               d="M24 64.5C24 52.6 33.6 43 45.5 43H67v43H45.5C33.6 86 24 76.4 24 64.5zm0 0"
             ></path>
             <path
-              className="fill-transition group-hover:fill-tech-figma-300 group-hover:stroke-tech-figma-300"
+              className="fill-transition group-hover:fill-tech-figma-300"
               fill="#272727"
               stroke="#fafafa"
               d="M24 21.5C24 9.6 33.6 0 45.5 0H67v43H45.5C33.6 43 24 33.4 24 21.5zm0 0"
             ></path>
             <path
-              className="fill-transition group-hover:fill-tech-figma-400 group-hover:stroke-tech-figma-400"
+              className="fill-transition group-hover:fill-tech-figma-400"
               fill="#272727"
               stroke="#fafafa"
               d="M67 0h21.5C100.4 0 110 9.6 110 21.5S100.4 43 88.5 43H67zm0 0"
             ></path>
             <path
-              className="fill-transition group-hover:fill-tech-figma-500 group-hover:stroke-tech-figma-500"
+              className="fill-transition group-hover:fill-tech-figma-500"
               fill="#272727"
               stroke="#fafafa"
               d="M110 64.5c0 11.9-9.6 21.5-21.5 21.5S67 76.4 67 64.5 76.6 43 88.5 43 110 52.6 110 64.5zm0 0"
