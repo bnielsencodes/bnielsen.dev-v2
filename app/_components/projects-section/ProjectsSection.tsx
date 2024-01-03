56bc06eb7e22b7c411238497a0c5a686407bd506
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:19:28 -0800

    Utilize next-themes for light/dark theme rather than prop-drilling

--- app/_components/projects-section/ProjectsSection.tsx
+++ app/_components/projects-section/ProjectsSection.tsx
@@ -1,30 +1,18 @@
 import { projects } from "@/app/data";
 import Project from "./Project";
-import clsx from "clsx";
 
-export default function ProjectsSection({ darkMode }: { darkMode: boolean }) {
+export default function ProjectsSection() {
   const projectElements = projects.map((project) => {
-    return <Project key={project.id} {...{ darkMode, project }} />;
+    return <Project key={project.id} {...{ project }} />;
   });
 
   return (
     <section
       id="projects-section"
-      className={clsx("pt-20 overflow-x-clip", {
-        "bg-neutral-100": darkMode,
-        "bg-neutral-600": !darkMode,
-      })}
+      className="pt-20 bg-neutral-600 overflow-x-clip dark:bg-neutral-100"
     >
       {/* section heading */}
-      <h2
-        className={clsx(
-          "relative z-0 w-[130px] mx-auto text-[2.5rem] font-bold text-center lg:w-[143px] lg:text-[2.75rem] after:absolute after:content-[''] after:-z-10 after:right-0 after:bottom-[5px] after:w-[93px] after:h-[14px] lg:after:w-[101px] lg:after:bottom-[7px]",
-          {
-            "text-neutral-500 after:bg-neutral-300": darkMode,
-            "text-neutral-200 after:bg-neutral-400": !darkMode,
-          }
-        )}
-      >
+      <h2 className="relative z-0 w-[130px] mx-auto text-neutral-200 text-[2.5rem] font-bold text-center lg:w-[143px] lg:text-[2.75rem] after:absolute after:content-[''] after:-z-10 after:right-0 after:bottom-[5px] after:w-[93px] after:h-[14px] after:bg-neutral-400 dark:text-neutral-500 dark:after:bg-neutral-300 lg:after:w-[101px] lg:after:bottom-[7px]">
         Work
       </h2>
       <div className="flex flex-col justify-center pb-[25px] mt-[165px] text-[0.9rem] lg:mt-[260px] lg:pb-0">
