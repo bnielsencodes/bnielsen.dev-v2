40ac6a37fb93c2fb7fa2f79e814bc049cc4acc43
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:18:22 -0800

    Utilize next-themes for light/dark theme rather than prop-drilling

--- app/_components/about-section/bio/Bio.tsx
+++ app/_components/about-section/bio/Bio.tsx
@@ -1,52 +1,30 @@
-import clsx from "clsx";
 import Avatar from "../../shared/Avatar";
 
-export default function Bio({ darkMode }: { darkMode: boolean }) {
+export default function Bio() {
   return (
     <div className="flex flex-col items-center gap-8 mx-auto mb-24 md:gap-12 md:w-[720px] md:mb-16 lg:flex-row-reverse lg:gap-10 lg:w-[735px] lg:mx-auto lg:mb-32 xl:flex-col xl:w-[605px] xl:mx-0 xl:mb-0">
       <Avatar parent="bio" />
 
       <div className="mt-4 font-sans font-light md:text-[1.063rem]">
         {/* statement copy */}
-        <p
-          className={clsx(
-            "font-mosk text-5xl font-extrabold leading-[1.1em] xl:text-[3.5rem] xl:leading-[3.875rem]",
-            { "text-neutral-500": darkMode, "text-neutral-200": !darkMode }
-          )}
-        >
+        <p className="text-neutral-200 font-mosk text-5xl font-extrabold leading-[1.1em] dark:text-neutral-500 xl:text-[3.5rem] xl:leading-[3.875rem]">
           I like to create visually stunning and user-friendly websites for
           businesses of all sizes.
         </p>
 
         {/* currently open for work */}
-        <p
-          className={clsx("mt-8 mb-5 text-2xl font-light tracking-wide", {
-            "text-neutral-600": darkMode,
-            "text-neutral-100": !darkMode,
-          })}
-        >
+        <p className="mt-8 mb-5 text-neutral-100 text-2xl font-light tracking-wide dark:text-neutral-600">
           Currently open to freelance and full-time opportunities.
         </p>
 
         {/* about me copy */}
-        <p
-          className={clsx("mb-4 text-[1.125rem]", {
-            "text-neutral-500": darkMode,
-            "text-neutral-200": !darkMode,
-          })}
-        >
+        <p className="mb-4 text-neutral-200 text-[1.125rem] dark:text-neutral-500">
           With a background in behavioral health, I take a user-centered
           approach to building websites, putting an emphasis on accessibility
           and creating a smooth seamless user experience. If you have an idea
           you think I could help bring to life,{" "}
           <a
-            className={clsx(
-              "opacity-transition rounded-sm focus:opacity-80 lg:hover:opacity-80",
-              {
-                "outline-dark-theme text-accent-200": darkMode,
-                "outline-light-theme text-accent-100": !darkMode,
-              }
-            )}
+            className="outline-light-theme opacity-transition rounded-sm text-accent-100 dark:outline-dark-theme dark:text-accent-200 focus:opacity-80 lg:hover:opacity-80"
             href="#contact-section"
           >
             feel free to reach out and let&apos;s talk
@@ -55,13 +33,7 @@ export default function Bio({ darkMode }: { darkMode: boolean }) {
           playing video games, spending time with family, enjoying the outdoors,
           or lurking on the{" "}
           <a
-            className={clsx(
-              "opacity-transition rounded-sm focus:opacity-80 lg:hover:opacity-80",
-              {
-                "outline-dark-theme text-accent-200": darkMode,
-                "outline-light-theme text-accent-100": !darkMode,
-              }
-            )}
+            className="outline-light-theme opacity-transition rounded-sm text-accent-100 dark:outline-dark-theme dark:text-accent-200 focus:opacity-80 lg:hover:opacity-80"
             href="https://reddit.com"
             target="_blank"
           >
