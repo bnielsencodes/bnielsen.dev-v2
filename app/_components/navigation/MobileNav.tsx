84cf00615d525728cf1f202de740afa413fbd98d
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:13:58 -0800

    Utilize next-themes for light/dark theme rather than prop-drilling

--- app/_components/navigation/MobileNav.tsx
+++ app/_components/navigation/MobileNav.tsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 import Socials from "@/app/_components/socials/Socials";
 import ThemeToggle from "./ThemeToggle";
-import clsx from "clsx";
 
 const variants = {
   open: { opacity: 1, x: 0 },
@@ -9,25 +8,15 @@ const variants = {
 };
 
 export default function MobileNav({
-  darkMode,
-  toggleTheme,
   showMobileNav,
   toggleMobileNav,
 }: {
-  darkMode: boolean;
-  toggleTheme: () => void;
   showMobileNav: boolean;
   toggleMobileNav: () => void;
 }) {
   return (
     <motion.div
-      className={clsx(
-        "fixed top-0 right-0 left-0 z-40 flex flex-col h-screen",
-        {
-          "bg-neutral-100": darkMode,
-          "bg-neutral-600": !darkMode,
-        }
-      )}
+      className="fixed top-0 right-0 left-0 z-40 flex flex-col h-screen bg-neutral-600 dark:bg-neutral-100"
       animate={showMobileNav ? "open" : "closed"}
       variants={variants}
       initial={{ opacity: 0, translateX: "-20rem" }}
@@ -39,15 +28,7 @@ export default function MobileNav({
     >
       {/* top container */}
       <div className="flex items-center justify-start w-full h-full pl-4 md:pl-5">
-        <ul
-          className={clsx(
-            "flex flex-col justify-between h-[17.5rem] pb-[25px] text-[4.85rem] md:h-[19rem] md:text-[5.5rem]",
-            {
-              "text-neutral-500": darkMode,
-              "text-neutral-200": !darkMode,
-            }
-          )}
-        >
+        <ul className="flex flex-col justify-between h-[17.5rem] pb-[25px] text-neutral-200 text-[4.85rem] dark:text-neutral-500 md:h-[19rem] md:text-[5.5rem]">
           <motion.div
             className="flex items-center h-[83px]"
             animate={showMobileNav ? "open" : "closed"}
@@ -63,10 +44,7 @@ export default function MobileNav({
             {/* about link */}
             <li className="flex items-center h-[83px]">
               <a
-                className={clsx("font-extrabold", {
-                  "active:text-accent-200": darkMode,
-                  "active:text-accent-100": !darkMode,
-                })}
+                className="font-extrabold active:text-accent-100 dark:active:text-accent-200"
                 onClick={() => toggleMobileNav()}
                 href="#about-section"
                 aria-label="about"
@@ -91,10 +69,7 @@ export default function MobileNav({
             {/* portfolio link */}
             <li className="flex items-center h-[83px]">
               <a
-                className={clsx("font-extrabold", {
-                  "active:text-accent-200": darkMode,
-                  "active:text-accent-100": !darkMode,
-                })}
+                className="font-extrabold active:text-accent-100 dark:active:text-accent-200"
                 onClick={() => toggleMobileNav()}
                 href="#projects-section"
                 aria-label="projects"
@@ -119,10 +94,7 @@ export default function MobileNav({
             {/* contact link */}
             <li className="flex items-center h-[83px]">
               <a
-                className={clsx("font-extrabold", {
-                  "active:text-accent-200": darkMode,
-                  "active:text-accent-100": !darkMode,
-                })}
+                className="font-extrabold active:text-accent-100 dark:active:text-accent-200"
                 onClick={() => toggleMobileNav()}
                 href="#contact-section"
                 aria-label="contact"
@@ -135,9 +107,9 @@ export default function MobileNav({
       </div>
 
       {/* bottom container */}
-      <div className="flex items-center justify-between w-full h-[75px] pl-4 pr-2 pb-4 md:pl-5 md:pr-3">
-        <Socials parent="mobile nav" direction="row" {...{ darkMode }} />
-        <ThemeToggle {...{ darkMode, toggleTheme }} />
+      <div className="flex items-center justify-between w-full h-[75px] pl-4 pr-[29px] pb-4 md:pl-5 md:pr-3">
+        <Socials parent="mobile nav" direction="row" />
+        <ThemeToggle />
       </div>
     </motion.div>
   );
