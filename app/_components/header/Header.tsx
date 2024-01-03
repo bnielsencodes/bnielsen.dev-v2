5b4559619b0abb1b1252e088a6af908487d8a0e2
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:17:11 -0800

    Utilize next-themes for light/dark theme rather than prop-drilling

--- app/_components/header/Header.tsx
+++ app/_components/header/Header.tsx
@@ -1,133 +1,106 @@
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 import Avatar from "../shared/Avatar";
 import CTAButtons from "./CTAButtons";
 import Socials from "@/app/_components/socials/Socials";
-import clsx from "clsx";
 
-export default function Header({ darkMode }: { darkMode: boolean }) {
-  return (
-    <header
-      id="header"
-      className="relative flex flex-col items-start justify-center h-[100svh] pt-[56px] px-[17px] mb-[6px] md:px-[19px] lg:items-center lg:pl-[22px]"
-    >
-      {/* Avatar image on larger screens */}
-      <Avatar parent="header" />
+export default function Header() {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme } = useTheme();
 
-      {/* header intro */}
-      <h1
-        className={clsx(
-          "all-transition w-full mt-5 mb-5 text-[11vw] font-extrabold leading-[1.15em] tracking-wide text-left md:text-[7.6vw] lg:w-[850px] lg:mt-[1.6rem] lg:text-[4.125rem] lg:leading-[1.1em] lg:text-center xl:w-[985px] xl:text-[76px] xl:font-extrabold xl:leading-[5.35rem]",
-          {
-            "text-neutral-600": darkMode,
-            "text-neutral-100": !darkMode,
-          }
-        )}
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <header
+        id="header"
+        className="relative flex flex-col items-start justify-center h-[100svh] pt-[56px] px-[17px] mb-[6px] md:px-[19px] lg:items-center lg:pl-[22px]"
       >
-        Hello, I&apos;m Brandon Nielsen, a{" "}
-        <span
-          className={clsx("all-transition", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          W
-        </span>
-        <span
-          className={clsx("all-transition", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          e
-        </span>
-        <span
-          className={clsx("all-transition", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          b
-        </span>{" "}
-        <span
-          className={clsx("all-transition", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          D
-        </span>
-        <span
-          className={clsx("all-transition", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          e
-        </span>
-        <span
-          className={clsx("all-transition", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          v
-        </span>
-        <span
-          className={clsx("", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          e
-        </span>
-        <span
-          className={clsx("all-transition", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          l
-        </span>
-        <span
-          className={clsx("all-transition", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          o
-        </span>
-        <span
-          className={clsx("all-transition", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          p
-        </span>
-        <span
-          className={clsx("all-transition", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          e
-        </span>
-        <span
-          className={clsx("all-transition", {
-            "header-text-stroke--dark": darkMode,
-            "header-text-stroke": !darkMode,
-          })}
-        >
-          r
-        </span>
-        .
-      </h1>
+        {/* Avatar image on larger screens */}
+        <Avatar parent="header" />
 
-      <CTAButtons {...{ darkMode }} />
+        {/* large screen socials */}
+        <div className="hidden lg:absolute lg:bottom-4 lg:right-6 lg:block">
+          <Socials parent="header" direction="column" />
+        </div>
+      </header>
+    );
+  }
 
-      {/* large screen socials */}
-      <div className="hidden lg:absolute lg:bottom-4 lg:right-6 lg:block">
-        <Socials parent="header" direction="column" {...{ darkMode }} />
-      </div>
-    </header>
-  );
+  if (resolvedTheme === "dark") {
+    return (
+      <header
+        id="header"
+        className="relative flex flex-col items-start justify-center h-[100svh] pt-[56px] px-[17px] mb-[6px] md:px-[19px] lg:items-center lg:pl-[22px]"
+      >
+        {/* Avatar image on larger screens */}
+        <Avatar parent="header" />
+
+        {/* header intro */}
+        <h1 className="all-transition w-full mb-5 text-neutral-100 text-[11vw] font-extrabold leading-[1.15em] tracking-wide text-left dark:text-neutral-600 md:text-[7.6vw] lg:w-[850px] lg:mt-[1.6rem] lg:text-[4.125rem] lg:leading-[1.1em] lg:text-center xl:w-[985px] xl:text-[76px] xl:font-extrabold xl:leading-[5.35rem]">
+          Hello, I&apos;m Brandon Nielsen, a{" "}
+          <span className="header-text-stroke--dark all-transition">W</span>
+          <span className="header-text-stroke--dark all-transition">e</span>
+          <span className="header-text-stroke--dark all-transition">
+            b
+          </span>{" "}
+          <span className="header-text-stroke--dark all-transition">D</span>
+          <span className="header-text-stroke--dark all-transition">e</span>
+          <span className="header-text-stroke--dark all-transition">v</span>
+          <span className="header-text-stroke--dark all-transition">e</span>
+          <span className="header-text-stroke--dark all-transition">l</span>
+          <span className="header-text-stroke--dark all-transition">o</span>
+          <span className="header-text-stroke--dark all-transition">p</span>
+          <span className="header-text-stroke--dark all-transition">e</span>
+          <span className="header-text-stroke--dark all-transition">r</span>.
+        </h1>
+
+        <CTAButtons />
+
+        {/* large screen socials */}
+        <div className="hidden lg:absolute lg:bottom-4 lg:right-6 lg:block">
+          <Socials parent="header" direction="column" />
+        </div>
+      </header>
+    );
+  }
+
+  if (resolvedTheme === "light") {
+    return (
+      <header
+        id="header"
+        className="relative flex flex-col items-start justify-center h-[100svh] pt-[56px] px-[17px] mb-[6px] md:px-[19px] lg:items-center lg:pl-[22px]"
+      >
+        {/* Avatar image on larger screens */}
+        <Avatar parent="header" />
+
+        {/* header intro */}
+        <h1 className="all-transition w-full mb-5 text-neutral-100 text-[11vw] font-extrabold leading-[1.15em] tracking-wide text-left dark:text-neutral-600 md:text-[7.6vw] lg:w-[850px] lg:mt-[1.6rem] lg:text-[4.125rem] lg:leading-[1.1em] lg:text-center xl:w-[985px] xl:text-[76px] xl:font-extrabold xl:leading-[5.35rem]">
+          Hello, I&apos;m Brandon Nielsen, a{" "}
+          <span className="header-text-stroke all-transition">W</span>
+          <span className="header-text-stroke all-transition">e</span>
+          <span className="header-text-stroke all-transition">b</span>{" "}
+          <span className="header-text-stroke all-transition">D</span>
+          <span className="header-text-stroke all-transition">e</span>
+          <span className="header-text-stroke all-transition">v</span>
+          <span className="header-text-stroke all-transition">e</span>
+          <span className="header-text-stroke all-transition">l</span>
+          <span className="header-text-stroke all-transition">o</span>
+          <span className="header-text-stroke all-transition">p</span>
+          <span className="header-text-stroke all-transition">e</span>
+          <span className="header-text-stroke all-transition">r</span>.
+        </h1>
+
+        <CTAButtons />
+
+        {/* large screen socials */}
+        <div className="hidden lg:absolute lg:bottom-4 lg:right-6 lg:block">
+          <Socials parent="header" direction="column" />
+        </div>
+      </header>
+    );
+  }
 }
