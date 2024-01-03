f0cb971b65dbe3953f23e2ad21e41f7ccb60b8e5
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:04:58 -0800

    Utilize next-themes for light/dark theme rather than prop-drilling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 import Image from "next/image";
 import waveDarkTop from "@/public/assets/images/section-dividers/wave-dark-top.svg";
 import waveLightTop from "@/public/assets/images/section-dividers/wave-light-top.svg";
@@ -11,75 +12,109 @@ import AboutSection from "./_components/about-section/AboutSection";
 import ProjectsSection from "./_components/projects-section/ProjectsSection";
 import ContactSection from "./_components/contact-section/ContactSection";
 import Footer from "./_components/footer/Footer";
-import clsx from "clsx";
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme } = useTheme();
 
-  //
-  //
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-  // prefers dark mode
-  // useEffect(() => {
-  //   const prefersDark = window.matchMedia(
-  //     "(prefers-color-scheme: dark)"
-  //   ).matches;
-  //   if (prefersDark) {
-  //     setDarkMode(true);
-  //   }
-  // }, []);
-
-  //
-  //
-
-  // toggle dark mode
-  function toggleTheme() {
-    setDarkMode((prevMode) => !prevMode);
+  if (!mounted) {
+    return (
+      <main className="bg-neutral-600 dark:bg-neutral-100">
+        <NavBar />
+        <Header />
+        <div className="w-full h-[81.5px] md:h-[170px] lg:h-[226.5px] xl:h-[319px]"></div>
+        <AboutSection />
+        <div className="w-full h-[83px] md:h-[169.25px] lg:h-[226.75px] xl:h-[319px]"></div>
+        <ProjectsSection />
+        <div className="w-full h-[82.5px] md:h-[169.7px] lg:h-[226.5px] xl:h-[319px]"></div>
+        <ContactSection />
+        <Footer />
+      </main>
+    );
   }
 
-  //
-  //
+  if (resolvedTheme === "dark") {
+    return (
+      <main className="bg-neutral-600 dark:bg-neutral-100">
+        <NavBar />
+        <Header />
+        {/* section top divider */}
+        <Image
+          className="w-full -mb-[1px]"
+          src={waveDarkTop}
+          alt="wave section divider"
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+        <AboutSection />
+        {/* section bottom divider */}
+        <Image
+          className="w-full -mt-[1px]"
+          src={waveDarkBottom}
+          alt="wave section divider"
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+        <ProjectsSection />
+        {/* section top divider */}
+        <Image
+          className="w-full -mb-[1px]"
+          src={waveDarkTop}
+          alt="wave section divider"
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+        <ContactSection />
+        <Footer />
+      </main>
+    );
+  }
 
-  return (
-    <main
-      className={clsx("", {
-        "bg-neutral-100": darkMode,
-        "bg-neutral-600": !darkMode,
-      })}
-    >
-      <NavBar {...{ darkMode, toggleTheme }} />
-      <Header {...{ darkMode }} />
-      {/* section top divider */}
-      <Image
-        className="w-full -mb-[1px]"
-        src={darkMode ? waveDarkTop : waveLightTop}
-        alt="wave section divider"
-        width="0"
-        height="0"
-        sizes="100vw"
-      />
-      <AboutSection {...{ darkMode }} />
-      {/* section bottom divider */}
-      <Image
-        className="w-full -mt-[1px]"
-        src={darkMode ? waveDarkBottom : waveLightBottom}
-        alt="wave section divider"
-        width="0"
-        height="0"
-        sizes="100vw"
-      />
-      <ProjectsSection {...{ darkMode }} />
-      {/* section top divider */}
-      <Image
-        className="w-full -mb-[1px]"
-        src={darkMode ? waveDarkTop : waveLightTop}
-        alt="wave section divider"
-        width="0"
-        height="0"
-        sizes="100vw"
-      />
-      <ContactSection {...{ darkMode }} />
-      <Footer {...{ darkMode }} />
-    </main>
-  );
+  if (resolvedTheme === "light") {
+    return (
+      <main className="bg-neutral-600 dark:bg-neutral-100">
+        <NavBar />
+        <Header />
+        {/* section top divider */}
+        <Image
+          className="w-full -mb-[1px]"
+          src={waveLightTop}
+          alt="wave section divider"
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+        <AboutSection />
+        {/* section bottom divider */}
+        <Image
+          className="w-full -mt-[1px]"
+          src={waveLightBottom}
+          alt="wave section divider"
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+        <ProjectsSection />
+        {/* section top divider */}
+        <Image
+          className="w-full -mb-[1px]"
+          src={waveLightTop}
+          alt="wave section divider"
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+        <ContactSection />
+        <Footer />
+      </main>
+    );
+  }
 }
