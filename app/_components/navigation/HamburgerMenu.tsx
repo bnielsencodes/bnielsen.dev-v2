878808673844c36f641b381a54122f48c75fe7c9
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:13:47 -0800

    Utilize next-themes for light/dark theme rather than prop-drilling

--- app/_components/navigation/HamburgerMenu.tsx
+++ app/_components/navigation/HamburgerMenu.tsx
@@ -1,11 +1,9 @@
 import clsx from "clsx";
 
 export default function HamburgerMenu({
-  darkMode,
   showMobileNav,
   toggleMobileNav,
 }: {
-  darkMode: boolean;
   showMobileNav: boolean;
   toggleMobileNav: () => void;
 }) {
@@ -14,12 +12,8 @@ export default function HamburgerMenu({
       className={clsx(
         "fixed top-2 right-0 z-50 flex flex-col justify-around items-end gap-1 w-[40px] h-[40px] py-3 px-2 -translate-x-3 md:-translate-x-4 group lg:hidden",
         {
-          "items-center -translate-x-[7px] transition-transform duration-500 md:-translate-x-[8px]":
+          "outline-light-theme transition-transform items-center border-2 border-neutral-200 -translate-x-[7px] duration-500 dark:outline-dark-theme dark:border-2 dark:border-neutral-500 md:-translate-x-[8px]":
             showMobileNav,
-          "outline-dark-theme border-2 border-neutral-500":
-            showMobileNav && darkMode,
-          "outline-light-theme border-2 border-neutral-200":
-            showMobileNav && !darkMode,
         }
       )}
       onClick={() => toggleMobileNav()}
@@ -30,12 +24,9 @@ export default function HamburgerMenu({
         className={clsx(
           "inline-block h-[2px] transition-transform duration-500",
           {
-            "w-6 -rotate-[135deg] translate-y-1": showMobileNav,
-            "w-7": !showMobileNav,
-            "bg-neutral-400": showMobileNav && darkMode,
-            "bg-neutral-300": showMobileNav && !darkMode,
-            "bg-neutral-600": !showMobileNav && darkMode,
-            "bg-neutral-100": !showMobileNav && !darkMode,
+            "w-6 bg-neutral-300 -rotate-[135deg] translate-y-1 dark:bg-neutral-400":
+              showMobileNav,
+            "w-7 bg-neutral-100 dark:bg-neutral-600": !showMobileNav,
           }
         )}
       ></span>
@@ -43,11 +34,9 @@ export default function HamburgerMenu({
         className={clsx(
           "inline-block w-6 h-[2px] transition-transform duration-500",
           {
-            "rotate-[135deg] -translate-y-1": showMobileNav,
-            "bg-neutral-400": showMobileNav && darkMode,
-            "bg-neutral-300": showMobileNav && !darkMode,
-            "bg-neutral-600": !showMobileNav && darkMode,
-            "bg-neutral-100": !showMobileNav && !darkMode,
+            "bg-neutral-300 rotate-[135deg] -translate-y-1 dark:bg-neutral-400":
+              showMobileNav,
+            "bg-neutral-100 dark:bg-neutral-600": !showMobileNav,
           }
         )}
       ></span>
