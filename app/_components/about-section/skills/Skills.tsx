16c6315e101446dc7c0e15c579fadb934c0b3d85
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:18:35 -0800

    Utilize next-themes for light/dark theme rather than prop-drilling

--- app/_components/about-section/skills/Skills.tsx
+++ app/_components/about-section/skills/Skills.tsx
@@ -1,28 +1,22 @@
 import useMediaQuery from "../../../_lib/useMediaQuery";
 import { skills, skillsLg } from "../../../data";
 import Skill from "./Skill";
-import clsx from "clsx";
 
-export default function Skills({ darkMode }: { darkMode: boolean }) {
+export default function Skills() {
   const isBreakpoint = useMediaQuery(768);
 
   return (
     <div className="flex flex-col justify-center gap-8 w-[293px] pb-[25px] mt-[50px] mx-auto text-[0.9rem] md:w-[420px] lg:w-[595px] xl:w-[293px] xl:mt-0 xl:mx-0">
-      <p
-        className={clsx("border-b-4  mx-auto font-bold text-[1.625rem]", {
-          "text-neutral-500 border-b-neutral-300": darkMode,
-          "text-neutral-200 border-b-neutral-400": !darkMode,
-        })}
-      >
+      <p className="border-b-4 border-b-neutral-400 mx-auto text-neutral-200 font-bold text-[1.625rem] dark:text-neutral-500 dark:border-b-neutral-300">
         Top Skills
       </p>
       <div className="grid grid-cols-3 gap-x-3 gap-y-4 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-3">
         {isBreakpoint
           ? skillsLg.map((skill) => {
-              return <Skill key={skill.id} {...{ skill, darkMode }} />;
+              return <Skill key={skill.id} {...{ skill }} />;
             })
           : skills.map((skill) => {
-              return <Skill key={skill.id} {...{ darkMode, skill }} />;
+              return <Skill key={skill.id} {...{ skill }} />;
             })}
       </div>
     </div>
