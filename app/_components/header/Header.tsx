0f53509adf6b58bf90cf764076a6dd0c7638808a
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:17:51 -0800

    Add loading skeleton

--- app/_components/header/Header.tsx
+++ app/_components/header/Header.tsx
@@ -22,6 +22,17 @@ export default function Header() {
         {/* Avatar image on larger screens */}
         <Avatar parent="header" />
 
+        <div className="w-full pl-[3px] mt-[6px] md:pl-[4.5px] md:mt-[15px] lg:mt-[46px] xl:mt-[34px]">
+          {/* header intro */}
+          <div className="flex flex-col justify-between gap-[5.1vw] mb-[28px] md:gap-[3.5vw] md:mb-[33px] lg:items-center lg:gap-[2.54vw] lg:mb-[32px] xl:gap-[33px] xl:max-w-[985px] xl:mx-auto xl:mb-[35px]">
+            <div className="w-[53%] pt-[8.5%] bg-neutral-400 animate-pulse rounded-lg dark:bg-neutral-500 md:hidden lg:h-[45px]"></div>
+            <div className="w-[96%] pt-[8.5%] bg-neutral-400 animate-pulse rounded-lg dark:bg-neutral-500 md:w-[70%] md:pt-[5.2%] lg:w-[88%] lg:pt-[4.4%] xl:w-full xl:pt-[5.8%]"></div>
+            <div className="w-[97%] pt-[8.5%] bg-neutral-400 animate-pulse rounded-lg dark:bg-neutral-500 md:w-[98%] md:pt-[5.2%] lg:w-[60%] lg:pt-[4.4%] xl:w-[65%] xl:pt-[5.8%]"></div>
+          </div>
+
+          <CTAButtons />
+        </div>
+
         {/* large screen socials */}
         <div className="hidden lg:absolute lg:bottom-4 lg:right-6 lg:block">
           <Socials parent="header" direction="column" />
