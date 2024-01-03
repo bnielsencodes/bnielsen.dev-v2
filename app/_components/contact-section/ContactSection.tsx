b51bb9ad57722b06214e8120f2999b1f6bdbf5c6
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:19:52 -0800

    Utilize next-themes for light/dark theme rather than prop-drilling

--- app/_components/contact-section/ContactSection.tsx
+++ app/_components/contact-section/ContactSection.tsx
@@ -1,28 +1,16 @@
 import ContactForm from "./ContactForm";
-import clsx from "clsx";
 
-export default function ContactSection({ darkMode }: { darkMode: boolean }) {
+export default function ContactSection() {
   return (
     <section
       id="contact-section"
-      className={clsx("pt-20 px-5 pb-28 overflow-x-clip", {
-        "bg-neutral-200": darkMode,
-        "bg-neutral-500": !darkMode,
-      })}
+      className="pt-20 px-5 pb-28 bg-neutral-500 overflow-x-clip dark:bg-neutral-200"
     >
       {/* section heading */}
-      <h2
-        className={clsx(
-          "relative z-0 w-[335px] mx-auto mb-[10px] text-[1.9rem] font-bold text-center md:w-[416px] md:text-[2.25rem] lg:w-[458px] lg:text-[2.5rem] after:absolute after:content-[''] after:-z-10 after:right-0 after:bottom-[6px] after:w-[293px] after:h-[9px] md:after:bottom-[8px] md:after:w-[358px] md:after:h-[10px] lg:after:bottom-[8px] lg:after:w-[397px] lg:after:h-[11px]",
-          {
-            "text-neutral-500 after:bg-neutral-300": darkMode,
-            "text-neutral-200 after:bg-neutral-400": !darkMode,
-          }
-        )}
-      >
+      <h2 className="relative z-0 w-[335px] mx-auto mb-[10px] text-neutral-200 text-[1.9rem] font-bold text-center after:bg-neutral-400 dark:text-neutral-500 dark:after:bg-neutral-300 md:w-[416px] md:text-[2.25rem] lg:w-[458px] lg:text-[2.5rem] after:absolute after:content-[''] after:-z-10 after:right-0 after:bottom-[6px] after:w-[293px] after:h-[9px] md:after:bottom-[8px] md:after:w-[358px] md:after:h-[10px] lg:after:bottom-[8px] lg:after:w-[397px] lg:after:h-[11px]">
         Want to work together?
       </h2>
-      <ContactForm {...{ darkMode }} />
+      <ContactForm />
     </section>
   );
 }
