4625079effc1fa0ee5282f590cd41dbaf60c345c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:19:41 -0800

    Utilize next-themes for light/dark theme rather than prop-drilling

--- app/_components/projects-section/ProjectButtons.tsx
+++ app/_components/projects-section/ProjectButtons.tsx
@@ -1,65 +1,115 @@
-import clsx from "clsx";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
 export default function ProjectButtons({
-  darkMode,
   liveSite,
   sourceCode,
 }: {
-  darkMode: boolean;
   liveSite: string;
   sourceCode: string;
 }) {
-  return (
-    <div className="relative flex gap-4 w-[261px]">
-      {/* live site button */}
-      <a
-        className={clsx("btn btn-primary focus:opacity-90", {
-          "btn-light text-neutral-200": darkMode,
-          "btn-dark text-neutral-500 shadow-xl": !darkMode,
-        })}
-        href={liveSite}
-        target="_blank"
-      >
-        Live Site
-      </a>
-      {/* live site HOVER button */}
-      <a
-        className={clsx("btn btn-primary absolute top-0 left-0", {
-          "btn-light-alt text-neutral-200": darkMode,
-          "btn-dark-alt text-neutral-500": !darkMode,
-        })}
-        href={liveSite}
-        target="_blank"
-        tabIndex={-1}
-        aria-hidden={true}
-      >
-        Live Site
-      </a>
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme } = useTheme();
 
-      {/* source code button */}
-      <a
-        className={clsx("btn btn-secondary focus:opacity-90", {
-          "btn-dark outline-dark-theme text-neutral-500": darkMode,
-          "btn-light outline-light-theme text-neutral-200 shadow-xl": !darkMode,
-        })}
-        href={sourceCode}
-        target="_blank"
-      >
-        Source Code
-      </a>
-      {/* source code HOVER button */}
-      <a
-        className={clsx("btn btn-secondary absolute top-0 right-[0.5px]", {
-          "btn-dark-alt outline-dark-theme text-neutral-500": darkMode,
-          "btn-light-alt outline-light-theme text-neutral-200": !darkMode,
-        })}
-        href={sourceCode}
-        target="_blank"
-        tabIndex={-1}
-        aria-hidden={true}
-      >
-        Source Code
-      </a>
-    </div>
-  );
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div className="flex">
+        <div className="w-[107px] h-[48px] mr-4 bg-neutral-400 animate-pulse rounded-lg dark:bg-neutral-500"></div>
+        <div className="w-[138px] h-[48px] bg-neutral-400 animate-pulse rounded-lg dark:bg-neutral-500"></div>
+      </div>
+    );
+  }
+
+  if (resolvedTheme === "dark") {
+    return (
+      <div className="relative flex gap-4 w-[261px]">
+        {/* live site button */}
+        <a
+          className="btn btn-primary btn-light text-neutral-200 shadow-xl focus:opacity-90"
+          href={liveSite}
+          target="_blank"
+        >
+          Live Site
+        </a>
+        {/* live site HOVER button */}
+        <a
+          className="btn btn-primary btn-light-alt absolute top-0 left-0 text-neutral-200"
+          href={liveSite}
+          target="_blank"
+          tabIndex={-1}
+          aria-hidden={true}
+        >
+          Live Site
+        </a>
+
+        {/* source code button */}
+        <a
+          className="btn btn-secondary btn-dark outline-dark-theme shadow-xl text-neutral-500 focus:opacity-90"
+          href={sourceCode}
+          target="_blank"
+        >
+          Source Code
+        </a>
+        {/* source code HOVER button */}
+        <a
+          className="btn btn-secondary btn-dark-alt outline-dark-theme absolute top-0 right-[0.5px] text-neutral-500"
+          href={sourceCode}
+          target="_blank"
+          tabIndex={-1}
+          aria-hidden={true}
+        >
+          Source Code
+        </a>
+      </div>
+    );
+  }
+
+  if (resolvedTheme === "light") {
+    return (
+      <div className="relative flex gap-4 w-[261px]">
+        {/* live site button */}
+        <a
+          className="btn btn-primary btn-dark shadow-xl text-neutral-500 focus:opacity-90"
+          href={liveSite}
+          target="_blank"
+        >
+          Live Site
+        </a>
+        {/* live site HOVER button */}
+        <a
+          className="btn btn-primary btn-dark-alt absolute top-0 left-0 text-neutral-500"
+          href={liveSite}
+          target="_blank"
+          tabIndex={-1}
+          aria-hidden={true}
+        >
+          Live Site
+        </a>
+
+        {/* source code button */}
+        <a
+          className="btn btn-secondary btn-light shadow-xl outline-light-theme text-neutral-200 focus:opacity-90"
+          href={sourceCode}
+          target="_blank"
+        >
+          Source Code
+        </a>
+        {/* source code HOVER button */}
+        <a
+          className="btn btn-secondary btn-light-alt outline-light-theme absolute top-0 right-[0.5px] text-neutral-200"
+          href={sourceCode}
+          target="_blank"
+          tabIndex={-1}
+          aria-hidden={true}
+        >
+          Source Code
+        </a>
+      </div>
+    );
+  }
 }
