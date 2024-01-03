2b51c4ad2359212599ca24c530fe4f5ee35ca092
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:02:43 -0800

    Update to use next-themes for light/dark theme
    Avoids prior prop-drilling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,72 +1,82 @@
 "use client";
 import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 import Link from "next/link";
-import clsx from "clsx";
 
 export default function NotFound() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme } = useTheme();
 
-  // prefers dark mode
-  // useEffect(() => {
-  //   const prefersDark = window.matchMedia(
-  //     "(prefers-color-scheme: dark)"
-  //   ).matches;
-  //   if (prefersDark) {
-  //     setDarkMode(true);
-  //   }
-  // }, []);
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-  return (
-    <div
-      className={clsx("global relative w-screen h-screen", {
-        "bg-neutral-100 text-neutral-500": darkMode,
-        "bg-neutral-600 text-neutral-200": !darkMode,
-      })}
-    >
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center w-full text-2xl font-extrabold">
-        <h1 className="text-7xl">404</h1>
-        <h2 className="mt-4 mb-6 text-4xl">Page Not Found</h2>
-        {/* button container */}
-        <div className="relative">
-          {/* default button */}
-          <button
-            className={clsx("btn btn-primary", {
-              "btn-light": darkMode,
-              "btn-dark": !darkMode,
-            })}
-          >
-            <Link
-              className={clsx("", {
-                "text-neutral-200": darkMode,
-                "text-neutral-500": !darkMode,
-              })}
-              href="/"
-            >
-            Return Home
-            </Link>
-          </button>
-  
-          {/* hover button */}
-          <button
-            className={clsx("btn btn-primary absolute top-0 left-0", {
-              "btn-light-alt": darkMode,
-              "btn-dark-alt": !darkMode,
-            })}
-          >
-            <Link
-              className={clsx("", {
-                "text-neutral-200": darkMode,
-                "text-neutral-500": !darkMode,
-              })}
-              href="/"
-              tabIndex={-1}
-              aria-hidden={true}
-            >
-            Return Home
-            </Link>
-          </button>
+  if (!mounted) {
+    return null;
+  }
+
+  if (resolvedTheme === "dark") {
+    return (
+      <div className="global relative w-screen h-screen bg-neutral-100 text-neutral-500">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center w-full text-2xl font-extrabold">
+          <h1 className="text-7xl">404</h1>
+          <h2 className="mt-4 mb-6 text-4xl">Page Not Found</h2>
+          {/* button container */}
+          <div className="relative">
+            {/* default button */}
+            <button className="btn btn-primary btn-light">
+              <Link className="text-neutral-200" href="/">
+                Return Home
+              </Link>
+            </button>
+
+            {/* hover button */}
+            <button className="btn btn-primary btn-light-alt absolute top-0 left-0">
+              <Link
+                className="text-neutral-200"
+                href="/"
+                tabIndex={-1}
+                aria-hidden={true}
+              >
+                Return Home
+              </Link>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  if (resolvedTheme === "light") {
+    return (
+      <div className="global relative w-screen h-screen bg-neutral-600 text-neutral-200">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center w-full text-2xl font-extrabold">
+          <h1 className="text-7xl">404</h1>
+          <h2 className="mt-4 mb-6 text-4xl">Page Not Found</h2>
+          {/* button container */}
+          <div className="relative">
+            {/* default button */}
+            <button className="btn btn-primary btn-dark">
+              <Link className="text-neutral-500" href="/">
+                Return Home
+              </Link>
+            </button>
+
+            {/* hover button */}
+            <button className="btn btn-primary btn-dark-alt absolute top-0 left-0">
+              <Link
+                className="text-neutral-500"
+                href="/"
+                tabIndex={-1}
+                aria-hidden={true}
+              >
+                Return Home
+              </Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
