e50367f864a22b6197e8b55108dd37e1148dea3f
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:18:15 -0800

    Utilize next-themes for light/dark theme rather than prop-drilling

--- app/_components/about-section/AboutSection.tsx
+++ app/_components/about-section/AboutSection.tsx
@@ -1,31 +1,19 @@
 import Bio from "./bio/Bio";
 import Skills from "./skills/Skills";
-import clsx from "clsx";
 
-export default function AboutSection({ darkMode }: { darkMode: boolean }) {
+export default function AboutSection() {
   return (
     <section
       id="about-section"
-      className={clsx("pt-20 px-[19px] pb-20 overflow-x-clip md:px-[21px]", {
-        "bg-neutral-200": darkMode,
-        "bg-neutral-500": !darkMode,
-      })}
+      className="pt-20 px-[19px] pb-20 bg-neutral-500 overflow-x-clip dark:bg-neutral-200 md:px-[21px]"
     >
       {/* section heading */}
-      <h2
-        className={clsx(
-          "relative z-0 w-[145px] mx-auto mb-[130px] text-[2.5rem] font-bold text-center md:mb-[145px] lg:w-[153px] lg:mb-[160px] lg:text-[2.75rem] after:absolute after:content-[''] after:-z-10 after:right-0 after:bottom-[5px] after:w-[104px] after:h-[14px] lg:after:bottom-[8px] lg:after:w-[111px]",
-          {
-            "text-neutral-500 after:bg-neutral-300": darkMode,
-            "text-neutral-200 after:bg-neutral-400": !darkMode,
-          }
-        )}
-      >
+      <h2 className="relative z-0 w-[145px] mx-auto mb-[130px] text-neutral-200 text-[2.5rem] font-bold text-center after:bg-neutral-400 dark:text-neutral-500 dark:after:bg-neutral-300 md:mb-[145px] lg:w-[153px] lg:mb-[160px] lg:text-[2.75rem] after:absolute after:content-[''] after:-z-10 after:right-0 after:bottom-[5px] after:w-[104px] after:h-[14px] lg:after:bottom-[8px] lg:after:w-[111px]">
         About
       </h2>
       <div className="xl:flex xl:items-center xl:justify-center xl:gap-56">
-        <Bio {...{ darkMode }} />
-        <Skills {...{ darkMode }} />
+        <Bio />
+        <Skills />
       </div>
     </section>
   );
