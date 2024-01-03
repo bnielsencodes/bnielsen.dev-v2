bbc9ec96a66dee3ff63118d3dc9207d4ec06c68b
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-02 21:21:20 -0800

    Utilize next-themes for light/dark theme rather than prop-drilling

--- app/_components/footer/Footer.tsx
+++ app/_components/footer/Footer.tsx
@@ -2,44 +2,27 @@ import ReturnHomeBtn from "./ReturnHomeBtn";
 import Location from "./Location";
 import ResumeBtn from "./ResumeBtn";
 import Socials from "../socials/Socials";
-import clsx from "clsx";
 
-export default function Footer({ darkMode }: { darkMode: boolean }) {
+export default function Footer() {
   return (
-    <footer
-      className={clsx(
-        "relative flex items-start justify-between h-40 pt-8 md:pr-[22px] md:pl-4 xl:pl-[17px]",
-        {
-          "bg-neutral-100": darkMode,
-          "bg-neutral-600": !darkMode,
-        }
-      )}
-    >
+    <footer className="relative flex items-start justify-between h-40 pt-8 bg-neutral-600 dark:bg-neutral-100 md:pr-[22px] md:pl-4 xl:pl-[17px]">
       <div className="absolute -top-5 left-1/2 -translate-x-1/2">
-        <ReturnHomeBtn {...{ darkMode }} />
+        <ReturnHomeBtn />
       </div>
 
       <div className="flex flex-col items-center justify-between h-full pb-4 mx-auto md:items-start md:mx-0">
         <div className="flex flex-col items-center justify-between gap-3 md:items-start">
-          <Location {...{ darkMode }} />
-          <ResumeBtn {...{ darkMode }} />
+          <Location />
+          <ResumeBtn />
         </div>
 
-        <small
-          className={clsx(
-            "pl-[7px] font-sans text-[0.725rem] uppercase xl:pl-2",
-            {
-              "text-neutral-400": darkMode,
-              "text-neutral-300": !darkMode,
-            }
-          )}
-        >
+        <small className="pl-[7px] text-neutral-300 font-sans text-[0.725rem] uppercase dark:text-neutral-400 xl:pl-2">
           Brandon Nielsen - 2023
         </small>
       </div>
 
       <div className="hidden md:block">
-        <Socials parent="footer" direction="row" {...{ darkMode }} />
+        <Socials parent="footer" direction="row" />
       </div>
     </footer>
   );
