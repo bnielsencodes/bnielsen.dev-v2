63e6959fa5ed7f3a58c4c883abcb9d9d4726495c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-08 01:20:30 -0800

    Update Footer to adapt to portfolio and blog pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import NavBar from "../_components/shared/navigation/NavBar";
+import Footer from "../_components/shared/footer/Footer";
+
 export default function Blog() {
   const blog = true;
   return (
     <main className="bg-neutral-600 dark:bg-neutral-100">
       <NavBar {...{ blog }} />
+      <Footer {...{ blog }} />
     </main>
   );
 }
