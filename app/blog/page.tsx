fb8515c012ae49a895ca9f02127b31e3a32352c2
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-08 01:17:42 -0800

    Import NavBar.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,10 @@
 "use client";
+import NavBar from "../_components/shared/navigation/NavBar";
 export default function Blog() {
+  const blog = true;
   return (
     <main className="bg-neutral-600 dark:bg-neutral-100">
+      <NavBar {...{ blog }} />
     </main>
   );
 }
