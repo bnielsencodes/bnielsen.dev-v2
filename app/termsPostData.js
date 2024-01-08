1f076e45364de7cf971255ae71d0e3cccb0c6843
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-08 01:12:05 -0800

    Create termsPostData.js

---
+++ app/termsPostData.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line
+export const post = {
+  title: "Web Related Terminology",
+  subtitle:
+    "A curated list of common web related terminology and their definitions",
+  date: "January 7th, 2023",
+};
