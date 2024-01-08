485e8589b54026546722c1fa438d619a65ccc5eb
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-08 01:23:19 -0800

    Import Content.tsx and TableOfContents.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import NavBar from "../_components/shared/navigation/NavBar";
 import Header from "../_components/blog/header/PostHeader";
+import Content from "../_components/blog/Content";
+import TableOfContents from "../_components/blog/TableOfContents";
 import Footer from "../_components/shared/footer/Footer";
 
 export default function Blog() {
@@ -9,6 +11,10 @@ export default function Blog() {
     <main className="bg-neutral-600 dark:bg-neutral-100">
       <NavBar {...{ blog }} />
       <Header />
+      <div className="flex gap-4 w-full pt-[120px] px-6 pb-4 mx-auto md:max-w-[686px] md:px-0 xl:max-w-[1100px] xl:px-8">
+        <Content />
+        <TableOfContents />
+      </div>
       <Footer {...{ blog }} />
     </main>
   );
