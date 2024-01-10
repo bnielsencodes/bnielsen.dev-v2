fa95b73a8af02279732dc661bf7efb4e6f394f66
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-10 00:49:37 -0800

    Add seeAlso elements

--- app/_components/blog/Term.tsx
+++ app/_components/blog/Term.tsx
@@ -27,10 +27,24 @@ export default function Term({
     return null;
   }
 
+  const seeAlso = term.seeAlso ? term.seeAlso : [];
+
+  const seeAlsoElements = seeAlso.map((term) => {
+    return (
+      <li
+        className="pr-[10px] text-neutral-300 dark:text-neutral-400 text-opacity-80 text-base leading-[22px] hover:text-opacity-70"
+        key={term.id}
+      >
+        <a href={term.link}>{term.name}</a>
+      </li>
+    );
+  });
+
   return (
     <li id={term.target} className="pt-16 -mb-7">
       <p className="text-2xl font-bold">{term.name}</p>
       <p className="pt-3 pb-2">{term.definition}</p>
+      <ul className="flex">{seeAlsoElements}</ul>
     </li>
   );
 }
