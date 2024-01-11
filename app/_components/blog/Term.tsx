70c39b99b9a3952fd2b5df9c048d73003eb53525
Brandon Nielsen <bnielsencodes@gmail.com>
2024-01-10 16:12:04 -0800

    Update padding

--- app/_components/blog/Term.tsx
+++ app/_components/blog/Term.tsx
@@ -43,9 +43,9 @@ export default function Term({
   return (
     <li id={term.target} className="pt-16 -mb-7">
       <p className="text-3xl font-bold">{term.name}</p>
-      <p className="pt-3 pb-4">{term.definition}</p>
+      <p className="pt-3">{term.definition}</p>
       <ul className="flex flex-wrap">
-        <span className="pr-[6px] text-neutral-300 dark:text-neutral-500 text-base leading-[22px]">
+        <span className="pt-4 pr-[6px] text-neutral-300 dark:text-neutral-500 text-base leading-[22px]">
           {term.seeAlso ? "See Also:" : null}
         </span>
         {seeAlsoElements}
