be40b6bcc25f8b7f8eb168a01a485a2e31750933
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-13 21:25:29 -0800

    Reformat

--- app/_components/shared/navigation/NavList.tsx
+++ app/_components/shared/navigation/NavList.tsx
@@ -1,15 +1,6 @@
-import { useState } from "react";
 import Link from "next/link";
-import clsx from "clsx";
 
 export default function NavList({ blog }: { blog?: boolean }) {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  // toggle categories dropdown
-  function toggleDropdown() {
-    setDropdownOpen((prevMode) => !prevMode);
-  }
-
   return (
     <>
       <ul className="flex justify-between">
@@ -35,23 +26,6 @@ export default function NavList({ blog }: { blog?: boolean }) {
 
         <li className="nav-item group">
           {blog ? (
-
-            //   <ul
-            //     className={clsx(
-            //       "all-transition absolute right-0 flex flex-wrap gap-x-4 gap-y-2 w-[150px] p-4 border-2 border-neutral-300 rounded-lg bg-neutral-600 text-[17px] font-medium dark:bg-neutral-300",
-            //       {
-            //         "top-[32px] block": dropdownOpen,
-            //         "top-0 hidden": !dropdownOpen,
-            //       }
-            //     )}
-            //   >
-            //     <li>React</li>
-            //     <li>Next.js</li>
-            //     <li>JavaScript</li>
-            //     <li>CSS</li>
-            //     <li>Career</li>
-            //   </ul>
-            // </div>
             <Link
               className="nav-link underline-light outline-light-theme underlineAnimation all-transition rounded-sm text-neutral-200 focus:opacity-80 group-hover:text-opacity-80 dark:underline-dark dark:outline-dark-theme dark:text-neutral-500"
               href="/blog/posts"
