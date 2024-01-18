e11b81fe336721cba65ef0cf0edcb59c5c055be9
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-17 22:11:22 -0800

    Update categoriesData.js

--- app/_data/blog/posts/categories/categoriesData.js
+++ app/_data/blog/posts/categories/categoriesData.js
@@ -1,17 +1,3 @@
-// import { categoryData as careerPostsData } from "@/app/_data/blog/posts/categories/career/categoryData";
-// import { categoryData as cssPostsData } from "@/app/_data/blog/posts/categories/css/categoryData";
-// import { categoryData as javascriptPostsData } from "@/app/_data/blog/posts/categories/javascript/categoryData";
-// import { categoryData as nextjsPostsData } from "@/app/_data/blog/posts/categories/nextjs/categoryData";
-// import { categoryData as reactPostsData } from "@/app/_data/blog/posts/categories/react/categoryData";
-// import { categoryData as tailwindPostsData } from "@/app/_data/blog/posts/categories/tailwind/categoryData";
 import { categoryData as webDevelopmentPostsData } from "@/app/_data/blog/posts/categories/web-development/categoryData";
 
-export const categoriesData = [
-  // ...careerPostsData,
-  // ...cssPostsData,
-  // ...javascriptPostsData,
-  // ...nextjsPostsData,
-  // ...reactPostsData,
-  // ...tailwindPostsData,
-  ...webDevelopmentPostsData,
-];
+export const categoriesData = [...webDevelopmentPostsData];
