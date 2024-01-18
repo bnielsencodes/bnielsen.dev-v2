694bfcfd43c26b8b538f707c77b4958ed06145b3
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-17 22:26:18 -0800

    Create page.tsx

---
+++ app/blog/posts/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { postsData } from "@/app/_data/blog/posts/postsData";
+import NavBar from "@/app/_components/blog/navigation/NavBar";
+import CategoryHeading from "@/app/_components/blog/posts/CategoryHeading";
+import PostTile from "@/app/_components/blog/posts/PostTile";
+import Footer from "@/app/_components/blog/footer/Footer";
+
+export default function Posts() {
+  const categories = postsData.categories.map((category) => {
+    if (category.posts.length > 0) {
+      return (
+        <div className="pb-[68px]" key={category.id}>
+          <CategoryHeading
+            title={category.title}
+            length={category.posts.length}
+          />
+          <ul className="flex flex-col gap-8 px-4 mt-5 text-neutral-300 dark:text-neutral-400 font-sans text-[14.5px] md:px-8 lg:grid lg:grid-cols-2 xl:px-0">
+            {category.posts.map((post) => {
+              return <PostTile key={post.id} {...{ post }} />;
+            })}
+          </ul>
+        </div>
+      );
+    }
+    return null;
+  });
+
+  return (
+    <main className="bg-neutral-600 dark:bg-neutral-100">
+      <NavBar />
+      <div className="pt-[192px] pb-[160px] lg:max-w-[1100px] lg:mx-auto">
+        {categories}
+      </div>
+      <Footer />
+    </main>
+  );
+}
