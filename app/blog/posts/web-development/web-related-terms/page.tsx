cb45c0955a324fb4f3413e0eef6e2496e2fa35f2
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-21 04:37:43 -0800

    Update import

--- app/blog/posts/web-development/web-related-terms/page.tsx
+++ app/blog/posts/web-development/web-related-terms/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 import NavBar from "@/app/_components/blog/navigation/NavBar";
 import Header from "@/app/_components/blog/posts/header/Header";
-import Content from "./_components/web-related-terms/Content";
+import TermsList from "./_components/web-related-terms/TermsList";
 import TableOfContents from "./_components/web-related-terms/TableOfContents";
 import Footer from "@/app/_components/blog/footer/Footer";
 
@@ -11,8 +11,8 @@ export default function WebRelatedTerms() {
     <main className="bg-neutral-600 dark:bg-neutral-100">
       <NavBar />
       <Header />
-      <div className="flex w-full gap-4 pt-[56px] px-6 pb-4 mx-auto md:max-w-[686px] md:px-0 xl:max-w-[1100px] xl:px-8">
-        <Content />
+      <div className="flex w-full gap-4 pt-[56px] px-4 pb-4 mx-auto md:max-w-[686px] md:px-0 xl:max-w-[1100px] xl:px-8">
+        <TermsList />
         <TableOfContents />
       </div>
       <Footer />
