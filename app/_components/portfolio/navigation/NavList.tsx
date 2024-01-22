a850a0a8f7828acb57a18ca7c9669bbff0dcb104
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-22 02:01:43 -0800

    Reformat

--- app/_components/portfolio/navigation/NavList.tsx
+++ app/_components/portfolio/navigation/NavList.tsx
@@ -2,48 +2,46 @@ import Link from "next/link";
 
 export default function NavList() {
   return (
-    <>
-      <ul className="flex justify-between">
-        <li className="nav-item group">
-          <a
-            className="nav-link underline-light dark:underline-dark underlineAnimation outline-light-theme dark:outline-dark-theme all-transition rounded-sm text-neutral-200 dark:text-neutral-500 focus:opacity-80 group-hover:text-opacity-80"
-            href="#about-section"
-            aria-label="about"
-          >
-            About
-          </a>
-        </li>
+    <ul className="flex justify-between">
+      <li className="nav-item group">
+        <a
+          className="nav-link underline-light dark:underline-dark underlineAnimation outline-light-theme dark:outline-dark-theme all-transition rounded-sm text-neutral-200 dark:text-neutral-500 focus:opacity-80 group-hover:text-opacity-80"
+          href="#about-section"
+          aria-label="about"
+        >
+          About
+        </a>
+      </li>
 
-        <li className="nav-item group">
-          <a
-            className="nav-link underline-light dark:underline-dark underlineAnimation outline-light-theme dark:outline-dark-theme all-transition rounded-sm text-neutral-200 dark:text-neutral-500 focus:opacity-80 group-hover:text-opacity-80"
-            href="#projects-section-heading"
-            aria-label="work"
-          >
-            Work
-          </a>
-        </li>
+      <li className="nav-item group">
+        <a
+          className="nav-link underline-light dark:underline-dark underlineAnimation outline-light-theme dark:outline-dark-theme all-transition rounded-sm text-neutral-200 dark:text-neutral-500 focus:opacity-80 group-hover:text-opacity-80"
+          href="#projects-section-heading"
+          aria-label="work"
+        >
+          Work
+        </a>
+      </li>
 
-        <li className="nav-item group">
-          <Link
-            className="nav-link underline-light dark:underline-dark underlineAnimation outline-light-theme dark:outline-dark-theme all-transition rounded-sm text-neutral-200 dark:text-neutral-500 focus:opacity-80 group-hover:text-opacity-80"
-            href="/blog"
-            aria-label="blog"
-          >
-            Blog
-          </Link>
-        </li>
+      <li className="nav-item group">
+        <Link
+          className="nav-link underline-light dark:underline-dark underlineAnimation outline-light-theme dark:outline-dark-theme all-transition rounded-sm text-neutral-200 dark:text-neutral-500 focus:opacity-80 group-hover:text-opacity-80"
+          href="/blog"
+          aria-label="blog"
+        >
+          Blog
+        </Link>
+      </li>
 
-        <li className="nav-item group">
-          <a
-            className="nav-link underline-light dark:underline-dark underlineAnimation outline-light-theme dark:outline-dark-theme all-transition rounded-sm text-neutral-200 dark:text-neutral-500 focus:opacity-80 group-hover:text-opacity-80"
-            href="#contact-section"
-            aria-label="contact"
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
-    </>
+      <li className="nav-item group">
+        <a
+          className="nav-link underline-light dark:underline-dark underlineAnimation outline-light-theme dark:outline-dark-theme all-transition rounded-sm text-neutral-200 dark:text-neutral-500 focus:opacity-80 group-hover:text-opacity-80"
+          href="#contact-section"
+          aria-label="contact"
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
   );
 }
