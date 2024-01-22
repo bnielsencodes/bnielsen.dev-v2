2c890bb5d71b1f645d149cf6069c461d3429ff11
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-21 21:49:45 -0800

    Reformat

--- app/blog/posts/web-development/web-related-terms/_components/Term.tsx
+++ app/blog/posts/web-development/web-related-terms/_components/Term.tsx
@@ -1,9 +1,8 @@
-import clsx from "clsx";
-
 export default function Term({
   term,
 }: {
   term: {
+    id: number;
     name: string;
     target: string;
     definition: string;
@@ -16,33 +15,27 @@ export default function Term({
     ];
   };
 }) {
-  const seeAlso = term.seeAlso ? term.seeAlso : [];
-
-  const seeAlsoElements = seeAlso.map((term) => {
-    return (
-      <li
-        className="pr-2 text-neutral-300 dark:text-neutral-400 text-base leading-[22px] text-opacity-80 hover:opacity-80"
-        key={term.id}
-      >
-        <a href={term.link}>{term.name}</a>
-      </li>
-    );
-  });
-
   return (
     <li id={term.target} className="pt-16">
       <p className="text-3xl font-bold">{term.name}</p>
       <p className="pt-3">{term.definition}</p>
-      <ul
-        className={clsx("flex flex-wrap pt-6", {
-          hidden: !term.seeAlso,
-        })}
-      >
-        <span className="pr-[6px] text-neutral-300 dark:text-neutral-500 text-base leading-[22px]">
-          {term.seeAlso ? "See Also:" : null}
-        </span>
-        {seeAlsoElements}
-      </ul>
+      {term.seeAlso && (
+        <ul className="flex flex-wrap pt-6">
+          <li className="min-w-[73.15px] pr-[6px] text-neutral-300 dark:text-neutral-500 text-base leading-[22px]">
+            See Also:
+          </li>
+          {term.seeAlso.map((term) => {
+            return (
+              <li
+                className="pr-2 text-neutral-300 dark:text-neutral-400 text-base leading-[22px] text-opacity-80 hover:opacity-80"
+                key={term.id}
+              >
+                <a href={term.link}>{term.name}</a>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </li>
   );
 }
