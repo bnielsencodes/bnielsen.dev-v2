d78662e7743af70d4a886a249438af89888c9d87
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-21 21:49:50 -0800

    Reformat

--- app/blog/posts/web-development/web-related-terms/_components/TermAccordion.tsx
+++ app/blog/posts/web-development/web-related-terms/_components/TermAccordion.tsx
@@ -2,46 +2,32 @@ import { useState } from "react";
 import Image from "next/image";
 import plusIcon from "@/public/assets/images/plusIcon.svg";
 import minusIcon from "@/public/assets/images/minusIcon.svg";
-import clsx from "clsx";
 
 export default function TermAccordion({
   term,
 }: {
   term: {
+    id: number;
     name: string;
-    target: string;
+    target?: string;
     definition: string;
     seeAlso?: [
       {
         id: number;
         name: string;
-        link: string;
+        link?: string;
       }
     ];
   };
 }) {
   const [isActive, setIsActive] = useState(false);
 
-  const seeAlso = term.seeAlso ? term.seeAlso : [];
-
-  const seeAlsoElements = seeAlso.map((term) => {
-    return (
-      <li
-        className="pr-2 text-neutral-300 dark:text-neutral-400 text-base leading-[22px] text-opacity-80 lg:hover:opacity-80"
-        key={term.id}
-      >
-        <a href={term.link}>{term.name}</a>
-      </li>
-    );
-  });
-
   return (
-    <li
-      id={term.target}
-      className="py-5 md:py-6 px-4 md:px-5 mt-4 rounded-lg bg-neutral-200"
-      onClick={() => setIsActive(!isActive)}
-    >
-      <div className="flex items-center justify-between gap-2">
+    <li className="py-5 md:py-7 px-4 md:px-7 mt-4 rounded-lg bg-neutral-200">
+      <div
+        className="flex items-center justify-between gap-2"
+        onClick={() => setIsActive(!isActive)}
+      >
         <p className="text-2xl font-bold lg:text-3xl">{term.name}</p>
         {isActive ? (
           <Image
@@ -66,16 +52,23 @@ export default function TermAccordion({
       {isActive && (
         <>
           <p className="pt-3">{term.definition}</p>
-          <ul
-            className={clsx("flex flex-wrap pt-6", {
-              hidden: !term.seeAlso,
-            })}
-          >
-            <span className="pr-[6px] text-neutral-300 dark:text-neutral-500 text-base leading-[22px]">
-              {term.seeAlso ? "See Also:" : null}
-            </span>
-            {seeAlsoElements}
-          </ul>
+          {term.seeAlso && (
+            <ul className="flex flex-wrap pt-6">
+              <li className="min-w-[73.15px] pr-[6px] text-neutral-300 dark:text-neutral-500 text-base leading-[22px]">
+                See Also:
+              </li>
+              {term.seeAlso.map((term) => {
+                return (
+                  <li
+                    className="pr-2 text-neutral-300 dark:text-neutral-400 text-base leading-[22px] text-opacity-80"
+                    key={term.id}
+                  >
+                    <p>{term.name}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
         </>
       )}
     </li>
