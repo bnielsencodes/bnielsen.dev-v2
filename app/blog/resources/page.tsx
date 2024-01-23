af383f0a203544304f211770648b23bd7a447621
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-22 21:06:41 -0800

    Create page.tsx

---
+++ app/blog/resources/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import NavBar from "@/app/_components/blog/navigation/NavBar";
+import Footer from "@/app/_components/blog/footer/Footer";
+
+export default function Resources() {
+  return (
+    <main className="bg-neutral-600 dark:bg-neutral-100">
+      <NavBar />
+      <Footer />
+    </main>
+  );
+}
