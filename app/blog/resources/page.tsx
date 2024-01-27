7c114319074ff0ea465d39f7ca35a6508a451542
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-26 23:58:42 -0800

    Add placeholder content

--- app/blog/resources/page.tsx
+++ app/blog/resources/page.tsx
@@ -6,6 +6,17 @@ export default function Resources() {
   return (
     <main className="bg-neutral-600 dark:bg-neutral-100">
       <NavBar />
+
+      {/* 
+        can remove min-h-screen below once there
+        are posts in this category
+      */}
+      <div className="min-h-screen pt-[192px] pb-[100px] lg:max-w-[1100px] lg:mx-auto">
+        <p className="px-4 mt-[200px] font-sans text-4xl text-center">
+          Content coming soon!
+        </p>
+      </div>
+
       <Footer />
     </main>
   );
