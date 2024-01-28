ee16873fd6490aee0b919c3d83d3a3b6d8d4beb8
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-27 17:57:27 -0800

    Update spacing

--- app/_components/blog/home/Header.tsx
+++ app/_components/blog/home/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
       className="flex items-center h-screen pt-[56px] px-5 bg-neutral-600 dark:bg-neutral-100 md:px-8 lg:px-0"
     >
       <div className="flex flex-col justify-center w-full max-w-[1100px] mx-auto lg:items-center">
-        <h1 className="mt-8 mb-[0.7rem] text-neutral-100 dark:text-neutral-600 text-[13vw] leading-[1.2] font-extrabold text-center lg:text-8xl lg:leading-[1.15]">
+        <h1 className="mt-8 mb-[0.7rem] text-neutral-100 dark:text-neutral-600 text-[13vw] leading-[1.2] font-extrabold text-center md:mb-6 lg:text-8xl lg:leading-[1.15]">
           A blog for web developers,
         </h1>
         <p className="text-neutral-200 dark:text-neutral-500 font-sans text-[20px] font-medium leading-[2rem] tracking-wide text-center md:text-[32px] md:leading-[1.35em] lg:text-3xl">
