45f3d9d8b0d3d6c908a4c776d79514a6cb51579b
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-01-28 22:00:09 -0800

    Reformat

--- app/blog/latest/page.tsx
+++ app/blog/latest/page.tsx
@@ -6,17 +6,16 @@ import PostTile from "@/app/_components/blog/posts/PostTile";
 import Footer from "../../_components/blog/footer/Footer";
 
 export default function Latest() {
-  const latestPosts = latestPostsData.map((post) => {
-    return <PostTile key={post.id} {...{ post }} />;
-  });
-
   return (
     <main className="blog dark:dark bg-neutral-600 dark:bg-neutral-100">
       <NavBar />
       <div className="pt-[192px] pb-[100px] lg:max-w-[1100px] lg:mx-auto">
-        <CategoryHeading title="Latest Posts" length={latestPosts.length} />
+        <CategoryHeading title="Latest Posts" length={latestPostsData.length} />
+
         <ul className="flex flex-col gap-8 px-4 mt-5 mb-[128px] text-neutral-300 dark:text-neutral-400 font-sans text-[14.5px] md:px-8 lg:grid lg:grid-cols-2 xl:px-0">
-          {latestPosts}
+          {latestPostsData.map((post) => {
+            return <PostTile key={post.id} {...{ post }} />;
+          })}
         </ul>
       </div>
       <Footer />
