caad33f4b87651ed6a96542461a90e56a7500fdf
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-02-02 17:37:41 -0800

    Update fetch url

--- app/_lib/ytvideos.js
+++ app/_lib/ytvideos.js
@@ -9,7 +9,7 @@ export default function YTvideos() {
   useEffect(() => {
     const fetchData = async () => {
       const results = await axios(
-        `https://youtube.googleapis.com/youtube/v3/channels?part=statistics&id=traversymedia&key=${youtubeApiKey}`
+        `https://youtube.googleapis.com/youtube/v3/channels?part=statistics&forHandle=traversymedia&key=${youtubeApiKey}`
       );
       setData(results.data);
       console.log(results.data);
