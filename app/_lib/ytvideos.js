50789a0c97500356991411086211750aa35dba58
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-02-02 16:26:38 -0800

    Create ytvideos.js

---
+++ app/_lib/ytvideos.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function YTvideos() {
+  const [data, setData] = useState();
+
+  const youtubeApiKey = process.env.YOUTUBE_API_KEY;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const results = await axios(
+        `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&channelId=traversymedia&key=${youtubeApiKey}`
+      );
+      setData(results.data);
+      console.log(results.data);
+    };
+    fetchData();
+  });
+
+  return (
+    <div>
+    </div>
+  );
+}
