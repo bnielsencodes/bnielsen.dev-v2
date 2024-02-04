1705c8d559df982d6ec4da65253c7362f45155f8
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-02-04 03:32:49 -0800

    Create Blog.tsx

---
+++ app/blog/resources/_components/blogs/Blog.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
+
+export default function YTChannel({
+}) {
+  return (
+    <li>
+      <Link
+        className="group flex flex-col md:flex-row md:items-center gap-[0.85rem] lg:gap-4"
+        href={blog.link}
+        target="_blank"
+      >
+        <Image
+          className="dark:hidden w-full md:w-1/2 lg:w-2/3 xl:w-3/5 rounded-lg lg:transition-opacity lg:group-hover:opacity-90"
+          src={blog.imgLight}
+          alt={`${blog.title} YouTube Channel avatar image`}
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+        <Image
+          className="hidden dark:block w-full md:w-1/2 lg:w-2/3 xl:w-3/5 rounded-lg lg:transition-opacity lg:group-hover:opacity-90"
+          src={blog.imgDark}
+          alt={`${blog.title} YouTube Channel avatar image`}
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+
+        <div className="flex flex-col gap-[0.65rem] pl-1">
+          <p className="text-[1.75rem] leading-[2.125rem] font-semibold lg:max-w-[250px] lg:transition-opacity lg:group-hover:opacity-80">
+            {blog.title}
+          </p>
+
+          <div className="flex flex-wrap gap-1">
+            {blog.tags.map((tag) => (
+              <span
+                className="py-1 px-2 rounded-md bg-neutral-300 text-neutral-200 dark:text-neutral-500 font-sans text-xs tracking-wide bg-opacity-10 dark:bg-opacity-30 lg:transition-opacity lg:group-hover:opacity-80"
+                key={tag.id}
+              >
+                {tag.name}
+              </span>
+            ))}
+          </div>
+        </div>
+      </Link>
+    </li>
+  );
+}
