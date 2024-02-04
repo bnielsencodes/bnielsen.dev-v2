6e2e9ec2e0519f1b79b1598f1252b3c791d7aead
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-02-03 23:49:42 -0800

    Update headings and padding

--- app/blog/resources/page.tsx
+++ app/blog/resources/page.tsx
@@ -9,14 +9,14 @@ export default function Resources() {
     <main className="blog dark:dark bg-neutral-600 dark:bg-neutral-100">
       <NavBar />
 
-      <div className="min-h-screen pt-[130px] px-4 pb-[160px] lg:max-w-[1100px] lg:px-8 lg:mx-auto">
+      <div className="min-h-screen pt-[130px] px-4 pb-[160px] md:px-8 lg:max-w-[1100px] lg:px-8 lg:mx-auto">
         {/* heading */}
-        <h1 className="mt-[200px] mb-3 font-monk text-5xl font-semibold">
+        <h1 className="mt-[200px] mb-3 font-monk text-5xl font-bold">
           Resources
         </h1>
 
         {/* subheading */}
-        <p className="font-sans text-xl">
+        <p className="text-neutral-500 font-sans text-xl">
           A curated list of resources to help unlock your greatest potential as
           a web developer
         </p>
