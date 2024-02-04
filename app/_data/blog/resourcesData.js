ad091111b217bf60fb4a1ed93583c168d33774ce
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-02-03 23:50:46 -0800

    Add youtube channels data

--- app/_data/blog/resourcesData.js
+++ app/_data/blog/resourcesData.js
@@ -2,6 +2,14 @@ import imgFutur from "@/public/assets/images/resources/youtube/the-futur.jpg";
 import imgTheo from "@/public/assets/images/resources/youtube/theo3gg.jpg";
 import imgFireship from "@/public/assets/images/resources/youtube/fireship.jpg";
 import imgTraversy from "@/public/assets/images/resources/youtube/traversy.jpg";
+import imgKevinPowell from "@/public/assets/images/resources/youtube/kevin-powell.jpg";
+import imgWebDevSimplified from "@/public/assets/images/resources/youtube/web-dev-simplified.jpg";
+import imgDesignCourse from "@/public/assets/images/resources/youtube/designcourse.jpg";
+import imgFluxAcademy from "@/public/assets/images/resources/youtube/flux-academy.jpg";
+import imgMosh from "@/public/assets/images/resources/youtube/programming-with-mosh.jpg";
+import imgAcademind from "@/public/assets/images/resources/youtube/academind.jpg";
+import imgScrimba from "@/public/assets/images/resources/youtube/scrimba.jpg";
+import imgSelfMadeMillennial from "@/public/assets/images/resources/youtube/self-made-millennial.jpg";
 
 // eslint-disable-next-line
 export const resources = {
@@ -46,7 +54,98 @@ export const resources = {
       tags: [
         { id: 1, name: "web development" },
         { id: 2, name: "programming" },
+        { id: 3, name: "tutorials" },
+      ],
+    },
+    {
+      id: 5,
+      title: "Kevin Powell",
+      link: "https://www.youtube.com/@KevinPowell",
+      img: imgKevinPowell,
+      tags: [
+        { id: 1, name: "web development" },
+        { id: 2, name: "css" },
+        { id: 3, name: "tutorials" },
+      ],
+    },
+    {
+      id: 6,
+      title: "Web Dev Simplified",
+      link: "https://www.youtube.com/@WebDevSimplified",
+      img: imgWebDevSimplified,
+      tags: [
+        { id: 1, name: "web development" },
+        { id: 2, name: "programming" },
+        { id: 3, name: "tutorials" },
+      ],
+    },
+    {
+      id: 7,
+      title: "DesignCourse",
+      link: "https://www.youtube.com/@DesignCourse",
+      img: imgDesignCourse,
+      tags: [
+        { id: 1, name: "web design" },
+        { id: 2, name: "css" },
+        { id: 3, name: "tutorials" },
+        { id: 4, name: "courses" },
+      ],
+    },
+    {
+      id: 8,
+      title: "Flux Academy",
+      link: "https://www.youtube.com/@FluxAcademy",
+      img: imgFluxAcademy,
+      tags: [
+        { id: 1, name: "web design" },
         { id: 2, name: "tutorials" },
+        { id: 3, name: "freelancing" },
+      ],
+    },
+    {
+      id: 9,
+      title: "Programming with Mosh",
+      link: "https://www.youtube.com/@programmingwithmosh",
+      img: imgMosh,
+      tags: [
+        { id: 1, name: "web development" },
+        { id: 2, name: "programming" },
+        { id: 3, name: "tutorials" },
+      ],
+    },
+    {
+      id: 10,
+      title: "Academind",
+      link: "https://www.youtube.com/@academind",
+      img: imgAcademind,
+      tags: [
+        { id: 1, name: "web development" },
+        { id: 2, name: "programming" },
+        { id: 3, name: "tutorials" },
+        { id: 4, name: "courses" },
+      ],
+    },
+    {
+      id: 11,
+      title: "Scrimba",
+      link: "https://www.youtube.com/@Scrimba",
+      img: imgScrimba,
+      tags: [
+        { id: 1, name: "web development" },
+        { id: 2, name: "tutorials" },
+        { id: 3, name: "courses" },
+        { id: 4, name: "career" },
+      ],
+    },
+    {
+      id: 12,
+      title: "Self Made Millennial",
+      link: "https://www.youtube.com/@SelfMadeMillennial",
+      img: imgSelfMadeMillennial,
+      tags: [
+        { id: 1, name: "interviewing" },
+        { id: 2, name: "career" },
+        { id: 3, name: "resume/cv" },
       ],
     },
   ],
