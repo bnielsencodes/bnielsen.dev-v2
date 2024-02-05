c13544b6ad8e94ad565d3972b000481a249752e5
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-02-04 22:15:16 -0800

    Create LearningPlatformsSection.tsx

---
+++ app/blog/resources/_components/learning-platforms/LearningPlatformsSection.tsx
@@ -0,0 +1,21 @@
+import { resources as resourcesData } from "@/app/_data/blog/resourcesData";
+
+export default function LearningPlatformsSection() {
+  return (
+    <section className="relative w-full border-t-2 border-neutral-200 dark:border-neutral-500 xl:max-w-[760px]">
+      <p
+        id="learning-platforms"
+        className="absolute top-[-22px] left-[10px] px-[6px] bg-neutral-600 dark:bg-neutral-100 text-3xl font-bold xl:top-[-97px] xl:pt-[74px]"
+      >
+        Learning Platforms
+      </p>
+
+      <ul
+        className="grid gap-6 max-w-[400px] mt-[50px] mx-auto md:grid-cols-2 md:gap-8 md:max-w-[785px] md:mt-[80px] lg:mt-20"
+        role="list"
+        aria-labelledby="learning-platforms"
+      >
+      </ul>
+    </section>
+  );
+}
