166e6ea84006f88904fee2d5f6fdd0ce09aa51e9
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-02-05 00:36:51 -0800

    Create ToolsSection.tsx

---
+++ app/blog/resources/_components/tools/ToolsSection.tsx
@@ -0,0 +1,21 @@
+import { resources as resourcesData } from "@/app/_data/blog/resourcesData";
+
+export default function ToolsSection() {
+  return (
+    <section className="relative w-full border-t-2 border-neutral-200 dark:border-neutral-500 xl:max-w-[760px]">
+      <p
+        id="tools"
+        className="absolute top-[-22px] left-[10px] px-[6px] bg-neutral-600 dark:bg-neutral-100 text-3xl font-bold xl:top-[-97px] xl:pt-[74px]"
+      >
+        Tools
+      </p>
+
+      <ul
+        className="flex flex-col gap-20 max-w-[400px] mt-[50px] mx-auto md:gap-16 md:max-w-[785px] md:mt-[80px] lg:gap-16 lg:mt-20"
+        role="list"
+        aria-labelledby="tools"
+      >
+      </ul>
+    </section>
+  );
+}
